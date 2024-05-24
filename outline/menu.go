6afc1b1ad6af92236eca7775a2067da4ca93b152
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-05-24 20:32:33 -0300

    feat: Add Go Playground option to menu

--- outline/menu.go
+++ outline/menu.go
@@ -3,6 +3,7 @@ package outline
 import (
 	"fmt"
 
+	variaveis_valores_tipos "github.com/fabianoflorentino/aprendago/outline/variaveis_valores_tipos"
 	visao_geral_do_curso "github.com/fabianoflorentino/aprendago/outline/visao_geral_do_curso"
 )
 
@@ -14,6 +15,7 @@ Opções:
   --sucesso                   Exibe a mensagem sobre sucesso
   --recursos                  Exibe os recursos do curso
   --como-esse-curso-funciona  Exibe como esse curso funciona
+  --go-playground             Exibe as informações do Go Playground
   --outline                   Exibe o outline do curso
   --help                      Exibe a lista de opções
 `
@@ -22,12 +24,13 @@ func Menu(args string) {
 	fmt.Println("Aprenda GO")
 
 	options := map[string]func(){
-		"--bem-vindo": func() { visao_geral_do_curso.BemVindo() },
-		"--porque-go": func() { visao_geral_do_curso.PorQueGo() },
-		"--sucesso":   func() { visao_geral_do_curso.Sucesso() },
-		"--recursos":  func() { visao_geral_do_curso.Recursos() },
-		"--outline":   func() { Outline() },
-		"--help":      func() { fmt.Print(HELPME) },
+		"--bem-vindo":     func() { visao_geral_do_curso.BemVindo() },
+		"--porque-go":     func() { visao_geral_do_curso.PorQueGo() },
+		"--sucesso":       func() { visao_geral_do_curso.Sucesso() },
+		"--recursos":      func() { visao_geral_do_curso.Recursos() },
+		"--go-playground": func() { variaveis_valores_tipos.GoPlayground() },
+		"--outline":       func() { Outline() },
+		"--help":          func() { fmt.Print(HELPME) },
 	}
 
 	if _, ok := options[args]; ok {
--- outline/outline.go
+++ outline/outline.go
@@ -1,7 +1,10 @@
 // OUTLINE: https://github.com/vkorbes/aprendago/blob/master/OUTLINE.md
 package outline
 
-import visao_geral_do_curso "github.com/fabianoflorentino/aprendago/outline/visao_geral_do_curso"
+import (
+	variaveis_valores_tipos "github.com/fabianoflorentino/aprendago/outline/variaveis_valores_tipos"
+	visao_geral_do_curso "github.com/fabianoflorentino/aprendago/outline/visao_geral_do_curso"
+)
 
 func Outline() {
 	visao_geral_do_curso.BemVindo()
@@ -9,4 +12,5 @@ func Outline() {
 	visao_geral_do_curso.Sucesso()
 	visao_geral_do_curso.Recursos()
 	visao_geral_do_curso.ComoEsseCursoFunciona()
+	variaveis_valores_tipos.GoPlayground()
 }
