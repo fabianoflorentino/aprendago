f539f5052ffd8ca62ce94fa47d77b8fdaf1fc751
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-05-24 17:49:52 -0300

    refactor: improve argument handling in main.go

--- main.go
+++ main.go
@@ -2,11 +2,19 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/fabianoflorentino/aprendago/outline"
 )
 
 func main() {
-	outline.Menu(os.Args[1])
+	if len(os.Args) < 2 {
+		fmt.Println("\nÉ necessário passar um argumento para o programa")
+		fmt.Println("\nExemplo: go run main.go --bem-vindo")
+		fmt.Print(outline.HELPME)
+		return
+	} else {
+		outline.Menu(os.Args[1])
+	}
 }
