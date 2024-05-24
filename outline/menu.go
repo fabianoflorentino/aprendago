2aee76532dc6f4d0b8c025df64da2f17f9ac4441
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-05-24 17:49:43 -0300

    refactor: update menu options formatting in outline/menu.go

--- outline/menu.go
+++ outline/menu.go
@@ -2,30 +2,24 @@ package outline
 
 import (
 	"fmt"
-	"os"
 
 	visao_geral_do_curso "github.com/fabianoflorentino/aprendago/outline/visao_geral_do_curso"
 )
 
 const HELPME = `
 Opções:
-	--bem-vindo   							Exibe a mensagem de boas-vindas do curso
-	--porque-go   							Exibe a mensagem sobre por que aprender Go
-	--sucesso     							Exibe a mensagem sobre sucesso
-	--recursos    							Exibe os recursos do curso
-	--como-esse-curso-funciona	Exibe como esse curso funciona
-	--outline     							Exibe o outline do curso
-	--help        							Exibe a lista de opções
+
+  --bem-vindo                 Exibe a mensagem de boas-vindas do curso
+  --porque-go                 Exibe a mensagem sobre por que aprender Go
+  --sucesso                   Exibe a mensagem sobre sucesso
+  --recursos                  Exibe os recursos do curso
+  --como-esse-curso-funciona  Exibe como esse curso funciona
+  --outline                   Exibe o outline do curso
+  --help                      Exibe a lista de opções
 `
 
 func Menu(args string) {
 	fmt.Println("Aprenda GO")
-	if len(os.Args) < 2 {
-		fmt.Println("\nÉ necessário passar um argumento para o programa")
-		fmt.Println("Exemplo: go run main.go --bem-vindo")
-		fmt.Print(HELPME)
-		return
-	}
 
 	options := map[string]func(){
 		"--bem-vindo": func() { visao_geral_do_curso.BemVindo() },
