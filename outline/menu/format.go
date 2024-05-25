2efee41f4f11dc55bd4d3c61f7a61c096cbaf80c
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-05-25 12:26:26 -0300

    refactor: Update menu options formatting in outline/menu.go

--- outline/menu/format.go
+++ outline/menu/format.go
@@ -3,6 +3,7 @@ package outline
 import (
 	"fmt"
 	"os"
+	"strings"
 	"text/template"
 )
 
@@ -14,10 +15,15 @@ import (
 // O código usa a função printf para formatar uma string, inserindo valores dinâmicos como .Width, .Flag e .Description.
 // O resultado formatado da string será impresso ou usado de alguma outra forma no restante do código.
 var templateHelpMe = `
+Uso: go run main.go [opção]
+
+Exemplos:
+  go run main.go --bem-vindo
+
 Opções:
 
 {{- range .}}
-  {{ printf "%-*s" .Width .Flag }}   {{ .Description }}
+  {{ printf "%-*s" .Width .Flag }}   {{ .Description | indent .Width}}
 {{- end }}
 `
 
@@ -63,6 +69,23 @@ func parseWidth(flags []HelpMe) int {
 	return maxWidth
 }
 
+// indent adiciona um recuo à frente de cada linha de uma string.
+// O recuo é calculado com base no tamanho da flag e em um valor fixo (4).
+func indent(width int, description string) string {
+	lines := strings.Split(description, "\n")
+	if len(lines) <= 1 {
+		return description
+	}
+
+	// O loop for percorre as linhas da descrição e adiciona um recuo à frente de cada linha.
+	// O recuo é calculado com base no tamanho da flag e em um valor fixo (4).
+	for idx := 1; idx < len(lines); idx++ {
+		lines[idx] = strings.Repeat(" ", width+4) + lines[idx]
+	}
+
+	return strings.Join(lines, "\n")
+}
+
 // PrintHelpMe é uma função que imprime a lista de opções disponíveis para o programa.
 // A função usa um template para formatar a saída de ajuda de maneira uniforme.
 func PrintHelpMe() {
@@ -75,6 +98,7 @@ func PrintHelpMe() {
 	// Neste caso, estamos usando a função printf do pacote fmt para formatar a saída.
 	funcMap := template.FuncMap{
 		"printf": fmt.Sprintf,
+		"indent": indent,
 	}
 
 	// tmpl é um objeto de template que contém o modelo de saída de ajuda.
