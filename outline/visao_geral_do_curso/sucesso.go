3b377026e3ee40c8af0f89b919a4255695361cc3
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-05-24 22:53:36 -0300

    refactor: Remove unnecessary code and improve formatting in sucesso.go

--- outline/visao_geral_do_curso/sucesso.go
+++ outline/visao_geral_do_curso/sucesso.go
@@ -5,25 +5,15 @@ import "fmt"
 func Sucesso() {
 	sucesso := `
 - Qual o motivo que algumas pessoa obtem sucesso e outras não?
-
 - Eu quero que você obtenha sucesso com este curso, então vamos falar sobre o assunto.
-
 - Perguntaram a Bill Gates e Warren Buffet, independentemente, qual seria sua principal característica responsável pelo seu sucesso. A resposta de ambos foi:
-
 - Foco.
-
 - (Fonte: https://www.linkedin.com/pulse/20140707144749-8353952-the-one-word-answer-to-why-bill-gates-and-warren-buffett-have-been-so-successful/)
-
 - O que isso quer dizer?
-
 - Escolha um objetivo e se concentre nele. Faça desse objetivo sua maior prioridade.
-
 - Foco. Concentração. → "Vou conseguir chegar lá ou vou morrer tentando."
-
 - Exemplo: minha carreira de violinista.
-
 - Faça disso uma prioridade, não uma resolução de ano novo. Tem vezes que seus amigos vão te chamar pra fazer algo massa e você vai ter que dizer, "gente, não vai dar, estou estudando."
-
 - O Todd costuma dizer: de gota em gota se enche um balde. A cada dia uma gota. Algumas semanas depois você olha o balde, e pô meu não tem quase nada ali, pra que esse esforço todo? Mas depois de alguns anos o balde vai estar transbordando. É só trabalhar, de gota em gota.
 
 - Então:
