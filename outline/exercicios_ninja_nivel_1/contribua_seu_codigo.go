4e06ad31460811628c6256d6adc05e10012f3cc0
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-05-26 12:13:21 -0300

    feat: Add ContribuaSeuCodigo() function to outline/exercicios_ninja_nivel_1

---
+++ outline/exercicios_ninja_nivel_1/contribua_seu_codigo.go
@@ -0,0 +1,16 @@
+package outline
+
+import "fmt"
+
+func ContribuaSeuCodigo() {
+	contribua_seu_codigo := `
+- Nesse curso a gente vai fazer um monte de exercícios.
+  - Talvez você queira contribuir suas próprias soluções.
+  - Talvez você tenha exemplos melhores que os que estamos mostrando aqui.
+- Para compartilhar me manda o link no twitter do seu código no Go Playground, twitter.com/ellenkorbes!
+- "@ellenkorbes Olha essa solução pro exercício Ninja nível 5, exercício 2: <link> O que vc acha?"
+  `
+
+	fmt.Println("Contribua seu código")
+	fmt.Println(contribua_seu_codigo)
+}
