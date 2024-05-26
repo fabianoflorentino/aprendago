25ce4aed1c57a2a177f7a4c2ba59b2dd18bc6195
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-05-26 18:41:34 -0300

    feat: Add NaPraticaExercicio1() function to outline/exercicios_ninja_nivel_1

---
+++ outline/exercicios_ninja_nivel_1/na_pratica_exercicio_1.go
@@ -0,0 +1,38 @@
+package outline
+
+import (
+	"fmt"
+)
+
+func NaPraticaExercicio1() {
+	na_pratica_exercicio_1 := `
+- Esses são seus primeiros exercícios, e seus primeiros passos.
+- Completando os exercícios dessa seção, você será um ninja nível 1.
+- É o seu primeiro passo pra se tornar um developer ninja.
+- Esses exercícios servem pra reforçar seu aprendizado. Só se aprende a programar programando. Ninguem aprende a andar de bicicleta assistindo vídeos de pessoas andando de bicicleta. É necessário botar a mão na massa.
+- Eu vou começar explicando qual é o exercício. Aí vou pedir pra você dar pausa. Esse é o momento de por os miolos pra trabalhar, encontrar sua solução, tec-tec-tec, e rodar pra ver se funciona. Depois é só dar play novamente, ver a minha abordagem para a mesma questão, e comparar nossas soluções.
+- Vamos lá:
+
+- Utilizando o operador curto de declaração, atribua estes valores às variáveis com os identificadores "x", "y", e "z".
+    1. 42
+    2. "James Bond"
+    3. true
+- Agora demonstre os valores nestas variáveis, com:
+    1. Uma única declaração print
+    2. Múltiplas declarações print
+- Solução: https://play.golang.org/p/yYXnWXIQNa
+  `
+
+	fmt.Println("Na prática: Exercicio #1")
+	fmt.Println(na_pratica_exercicio_1)
+}
+
+func ResolucaoNaPraticaExercicio1() {
+	x := 42
+	y := "James Bond"
+	z := true
+
+	fmt.Println("Valor de x:", x)
+	fmt.Println("Valor de y:", y)
+	fmt.Println("Valor de z:", z)
+}
