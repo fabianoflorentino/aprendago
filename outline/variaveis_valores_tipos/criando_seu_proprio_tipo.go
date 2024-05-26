ea522b053de00283f402fffd5691ef7d46c0dcd2
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-05-25 22:44:09 -0300

    feat: Improve formatting in outline/variaveis_valores_tipos/criando_seu_proprio_tipo.go

---
+++ outline/variaveis_valores_tipos/criando_seu_proprio_tipo.go
@@ -0,0 +1,18 @@
+package outline
+
+import "fmt"
+
+func CriandoSeuProprioTipo() {
+	criando_seu_proprio_tipo := `
+- Revisando: tipos em Go são extremamente importantes. (Veremos mais quando chegarmos em métodos e interfaces.)
+- Tem uma história que Bill Kennedy dizia que se um dia fizesse uma tattoo, ela diria "type is life."
+- Grande parte dos aspectos mais avançados de Go dependem quase que exclusivamente de tipos.
+- Como fundação para estas ferramentas, vamos aprender a declarar nossos próprios tipos.
+- Revisando: tipos são fixos. Uma vez declarada uma variável como de um certo tipo, isso é imutável.
+- type hotdog int → var b hotdog (main hotdog)
+- Uma variável de tipo hotdog não pode ser atribuida com o valor de uma variável tipo int, mesmo que este seja o tipo subjacente de hotdog.
+  `
+
+	fmt.Println("\nCriando seu proprio tipo")
+	fmt.Println(criando_seu_proprio_tipo)
+}
