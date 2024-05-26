a72e7f58fccd034619fa7c8c7d5cc188bd17c624
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-05-26 19:36:33 -0300

    feat: Add NaPraticaExercicio3 function to outline/exercicios_ninja_nivel_1/na_pratica_exercicio_3.go

---
+++ outline/exercicios_ninja_nivel_1/na_pratica_exercicio_3.go
@@ -0,0 +1,26 @@
+package outline
+
+import (
+	"fmt"
+)
+
+func NaPraticaExercicio3() {
+	na_pratica_exercicio_3 := `
+- Utilizando a solução do exercício anterior:
+  1. Em package-level scope, atribua os seguintes valores às variáveis:
+    1. para "x" atribua 42
+    2. para "y" atribua "James Bond"
+    3. para "z" atribua true
+  2. Na função main:
+    1. Use fmt.Sprintf para atribuir todos esses valores a uma única variável. Faça essa atribuição de tipo string a uma variável de nome "s" utilizando o operador curto de declaração.
+    2. Demonstre a variável "s".
+- Solução: https://play.golang.org/p/QFctSQB_h3
+  `
+
+	fmt.Println("Na prática: Exercicio #3")
+	fmt.Println(na_pratica_exercicio_3)
+}
+
+func ResolucaoNaPraticaExercicio3() {
+
+}
