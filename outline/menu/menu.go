3171553c1cea48e498edc71692e8dcfca24597b2
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-05-26 18:45:00 -0300

    Refactor outline.Menu() to accept a slice of arguments in main.go

--- outline/menu/menu.go
+++ outline/menu/menu.go
@@ -2,6 +2,7 @@ package outline
 
 import (
 	"fmt"
+	"strings"
 
 	outline "github.com/fabianoflorentino/aprendago/outline"
 	exercicios_ninja_nivel_1 "github.com/fabianoflorentino/aprendago/outline/exercicios_ninja_nivel_1"
@@ -9,29 +10,33 @@ import (
 	visao_geral_do_curso "github.com/fabianoflorentino/aprendago/outline/visao_geral_do_curso"
 )
 
-func Menu(args string) {
+func Menu(args []string) {
 	fmt.Println("Aprenda GO")
 
 	options := map[string]func(){
-		"--bem-vindo":                    func() { visao_geral_do_curso.BemVindo() },
-		"--porque-go":                    func() { visao_geral_do_curso.PorQueGo() },
-		"--sucesso":                      func() { visao_geral_do_curso.Sucesso() },
-		"--recursos":                     func() { visao_geral_do_curso.Recursos() },
-		"--go-playground":                func() { variaveis_valores_tipos.GoPlayground() },
-		"--hello-world":                  func() { variaveis_valores_tipos.HelloWorld() },
-		"--operador-curto-de-declaracao": func() { variaveis_valores_tipos.OperadorCurtoDeDeclaracao() },
-		"--a-palavra-reservada-var":      func() { variaveis_valores_tipos.ApalavraReservadaVar() },
-		"--explorando-tipos":             func() { variaveis_valores_tipos.ExplorandoTipos() },
-		"--valor-zero":                   func() { variaveis_valores_tipos.ValorZero() },
-		"--o-pacote-fmt":                 func() { variaveis_valores_tipos.OpacoteFmt() },
-		"--criando-seu-proprio-tipo":     func() { variaveis_valores_tipos.CriandoSeuProprioTipo() },
-		"--conversao-nao-coercao":        func() { variaveis_valores_tipos.ConversaoNaoCoercao() },
-		"--contribua-seu-codigo":         func() { exercicios_ninja_nivel_1.ContribuaSeuCodigo() },
-		"--outline":                      func() { outline.Outline() },
-		"--help":                         func() { PrintHelpMe() },
+		"--bem-vindo":                          func() { visao_geral_do_curso.BemVindo() },
+		"--porque-go":                          func() { visao_geral_do_curso.PorQueGo() },
+		"--sucesso":                            func() { visao_geral_do_curso.Sucesso() },
+		"--recursos":                           func() { visao_geral_do_curso.Recursos() },
+		"--go-playground":                      func() { variaveis_valores_tipos.GoPlayground() },
+		"--hello-world":                        func() { variaveis_valores_tipos.HelloWorld() },
+		"--operador-curto-de-declaracao":       func() { variaveis_valores_tipos.OperadorCurtoDeDeclaracao() },
+		"--a-palavra-reservada-var":            func() { variaveis_valores_tipos.ApalavraReservadaVar() },
+		"--explorando-tipos":                   func() { variaveis_valores_tipos.ExplorandoTipos() },
+		"--valor-zero":                         func() { variaveis_valores_tipos.ValorZero() },
+		"--o-pacote-fmt":                       func() { variaveis_valores_tipos.OpacoteFmt() },
+		"--criando-seu-proprio-tipo":           func() { variaveis_valores_tipos.CriandoSeuProprioTipo() },
+		"--conversao-nao-coercao":              func() { variaveis_valores_tipos.ConversaoNaoCoercao() },
+		"--contribua-seu-codigo":               func() { exercicios_ninja_nivel_1.ContribuaSeuCodigo() },
+		"--na-pratica-exercicio-1":             func() { exercicios_ninja_nivel_1.NaPraticaExercicio1() },
+		"--na-pratica-exercicio-1 --resolucao": func() { exercicios_ninja_nivel_1.ResolucaoNaPraticaExercicio1() },
+		"--outline":                            func() { outline.Outline() },
+		"--help":                               func() { PrintHelpMe() },
 	}
 
-	if action, ok := options[args]; ok {
+	argsStr := strings.Join(args, " ")
+
+	if action, ok := options[argsStr]; ok {
 		action()
 	} else {
 		fmt.Println("\nOpção inválida")
