90e0f9e290553efea0877a9102d6a82cc9fbd59b
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-05-28 00:19:24 -0300

    feat: Refactor NaPraticaExercicio1 code for improved readability and clarity

--- outline/exercicios_ninja_nivel_1/na_pratica_exercicio_1.go
+++ outline/exercicios_ninja_nivel_1/na_pratica_exercicio_1.go
@@ -10,7 +10,8 @@ func NaPraticaExercicio1() {
 - Completando os exercícios dessa seção, você será um ninja nível 1.
 - É o seu primeiro passo pra se tornar um developer ninja.
 - Esses exercícios servem pra reforçar seu aprendizado. Só se aprende a programar programando. Ninguem aprende a andar de bicicleta assistindo vídeos de pessoas andando de bicicleta. É necessário botar a mão na massa.
-- Eu vou começar explicando qual é o exercício. Aí vou pedir pra você dar pausa. Esse é o momento de por os miolos pra trabalhar, encontrar sua solução, tec-tec-tec, e rodar pra ver se funciona. Depois é só dar play novamente, ver a minha abordagem para a mesma questão, e comparar nossas soluções.
+- Eu vou começar explicando qual é o exercício. Aí vou pedir pra você dar pausa. Esse é o momento de por os miolos pra trabalhar, encontrar sua solução, tec-tec-tec, e rodar pra ver se funciona.
+  Depois é só dar play novamente, ver a minha abordagem para a mesma questão, e comparar nossas soluções.
 - Vamos lá:
 
 - Utilizando o operador curto de declaração, atribua estes valores às variáveis com os identificadores "x", "y", e "z".
