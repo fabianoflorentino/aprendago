c9e850e70484a142117db1d6c922560c733ad422
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-05-30 17:53:37 -0300

    feat: Add ComoOsComputadoresFuncionam function to outline/fundamentos_da_programacao/como_os_computadores_funcionam.go

---
+++ outline/fundamentos_da_programacao/como_os_computadores_funcionam.go
@@ -0,0 +1,16 @@
+package outline
+
+import "fmt"
+
+func ComoOsComputadoresFuncionam() {
+	como_os_computadores_funcionam := `
+- Isso é importante pois daqui pra frente vamos falar de ints, bytes, e etc.
+- Não é necessário um conhecimento a fundo mas é importante ter uma idéia de como as coisas funcionam por trás dos panos.
+- https://docs.google.com/presentation/d/1aVytiGOBVDMISFW-ZARJ5iFY1osU2XJIw0hQpNICXm8/
+- ASCII: https://en.wikipedia.org/wiki/ASCII
+- Filme: Alan Turing, The Immitation Game.
+  `
+
+	fmt.Println("Como os computadores funcionam")
+	fmt.Println(como_os_computadores_funcionam)
+}
