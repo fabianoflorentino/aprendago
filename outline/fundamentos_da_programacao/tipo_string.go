8cdbfa02ee35ba68062d6f88874047eebfb865f8
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-06-01 11:52:33 -0300

    feat: Add TipoString function to outline/fundamentos_da_programacao/tipo_string.go

---
+++ outline/fundamentos_da_programacao/tipo_string.go
@@ -0,0 +1,23 @@
+package outline
+
+import format "github.com/fabianoflorentino/aprendago/outline/format"
+
+func TipoString() {
+	topic := format.OutlineContent{
+		Title: "Tipo string",
+		Content: `
+- Strings são sequencias de bytes.
+- Imutáveis.
+- Uma string é um "slice of bytes" (ou, em português, uma fatia de bytes).
+- Na prática:
+  - %v %T
+  - Raw string literals
+  - Conversão para slice of bytes: []byte(x)
+  - %#U, %#x
+  - Go Playground: https://play.golang.org/p/dt2x1ies5b & https://play.golang.org/p/PpDnspiyA_7
+- https://blog.golang.org/string
+    `,
+	}
+
+	format.FormatOutlineTopic(topic)
+}
