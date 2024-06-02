a3791b2c12c6c041afd99970245775a52210de7c
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-06-01 21:14:55 -0300

    feat: Add Iota function to outline menu

---
+++ outline/fundamentos_da_programacao/iota.go
@@ -0,0 +1,18 @@
+package outline
+
+import format "github.com/fabianoflorentino/aprendago/outline/format"
+
+func Iota() {
+	topic := format.OutlineContent{
+		Title: "Iota",
+		Content: `
+- golang.org/ref/spec
+- Numa declaração de constantes, o identificador iota representa números sequenciais.
+- Na prática.
+  - iota, iota + 1, a = iota b c, reinicia em cada const, _
+- Go Playground: https://play.golang.org/p/eSrwoQjuYR
+    `,
+	}
+
+	format.FormatOutlineTopic(topic)
+}
