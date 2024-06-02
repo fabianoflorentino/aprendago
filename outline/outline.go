d02a33a0dda7ca58e75d35c04b77b15b9435d38b
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-06-01 21:44:53 -0300

    feat: Add ExerciciosNinjaNivel2 and NaPraticaExercicio1 Nivel2 functions to outline menu

--- outline/outline.go
+++ outline/outline.go
@@ -3,6 +3,7 @@ package outline
 
 import (
 	exercicios_ninja_nivel_1 "github.com/fabianoflorentino/aprendago/outline/exercicios_ninja_nivel_1"
+	exercicios_ninja_nivel_2 "github.com/fabianoflorentino/aprendago/outline/exercicios_ninja_nivel_2"
 	fundamentos_da_programacao "github.com/fabianoflorentino/aprendago/outline/fundamentos_da_programacao"
 	variaveis_valores_tipos "github.com/fabianoflorentino/aprendago/outline/variaveis_valores_tipos"
 	visao_geral_do_curso "github.com/fabianoflorentino/aprendago/outline/visao_geral_do_curso"
@@ -13,4 +14,5 @@ func Outline() {
 	variaveis_valores_tipos.VariaveisValoresETipos()
 	exercicios_ninja_nivel_1.ExerciciosNinjaNivel1()
 	fundamentos_da_programacao.FundamentosDaProgramacao()
+	exercicios_ninja_nivel_2.ExerciciosNinjaNivel2()
 }
