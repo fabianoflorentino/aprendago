8b970179d680a0b8c80e7286d6ded72e61c61467
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-06-02 12:03:08 -0300

    feat: Add NaPraticaExercicio4 function to outline menu

---
+++ outline/exercicios_ninja_nivel_2/na_pratica_exercicio_4.go
@@ -0,0 +1,33 @@
+package outline
+
+import (
+	"fmt"
+
+	format "github.com/fabianoflorentino/aprendago/outline/format"
+)
+
+func NaPraticaExercicio4() {
+	topic := format.OutlineContent{
+		Title: "Na Prática - Exercício #4",
+		Content: `
+- Crie um programa que:
+- Atribua um valor int a uma variável
+- Demonstre este valor em decimal, binário e hexadecimal
+- Desloque os bits dessa variável 1 para a esquerda, e atribua o resultado a outra variável
+- Demonstre esta outra variável em decimal, binário e hexadecimal
+- Solução: (https://play.golang.org/p/IiwgT0v3Mp) ou use no menu do programa (--na-pratica-exercicio-4 --nivel-2 --resolucao
+    `,
+	}
+
+	format.FormatOutlineTopic(topic)
+}
+
+func ResolucaoNaPraticaExercicio4() {
+	v1 := 10
+
+	fmt.Printf("Decimal: %d \nBinário: %b \nHexadecimal: %#x \n\n", v1, v1, v1)
+
+	v2 := v1 << 1
+
+	fmt.Printf("Decimal: %d \nBinário: %b \nHexadecimal: %#x", v2, v2, v2)
+}
