dfebeb79d95af939a9a0dfc7a1144466820f7843
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-06-02 12:39:38 -0300

    feat: Update link in NaPraticaExercicio6 function to include level option

--- outline/exercicios_ninja_nivel_1/na_pratica_exercicio_6.go
+++ outline/exercicios_ninja_nivel_1/na_pratica_exercicio_6.go
@@ -28,7 +28,7 @@ func NaPraticaExercicio6() {
 		Title: "Na Prática - Exercício #6",
 		Content: `
 - Prova!
-- Link: https://goo.gl/forms/s9y91iVSPvA4iahj1 [Link quebrado]. (Use a opção --na-pratica-exercicio-6 --prova)
+- Link: https://goo.gl/forms/s9y91iVSPvA4iahj1 [Link quebrado]. (Use a opção --na-pratica-exercicio-6 --nivel-1 --prova)
 - Se você deu pausa e fez todos os exercícios anteriores você mesmo, e só viu a resposta depois...
   e se você der pausa agora e fizer a prova inteira por conta própria, e só assistir as respostas depois...
   sabe o que isso quer dizer? Que você é ninja. Ninja nível 1. Tá no caminho certo pra ser um developer ninja mestre.
