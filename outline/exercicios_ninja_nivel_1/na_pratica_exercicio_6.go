234c656d7d28600582d069e4592e25b30b18df33
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-06-02 16:10:07 -0300

    refact: remove all formating functions and methods, move a new function format

--- outline/exercicios_ninja_nivel_1/na_pratica_exercicio_6.go
+++ outline/exercicios_ninja_nivel_1/na_pratica_exercicio_6.go
@@ -2,25 +2,10 @@
 package outline
 
 import (
-	"errors"
 	"fmt"
-	"strings"
 
 	format "github.com/fabianoflorentino/aprendago/outline/format"
 )
-
-// Prova representa uma pergunta da prova
-type Prova struct {
-	NumeroPergunta string
-	Perguntas      string
-	Opcoes         string
-}
-
-// Resposta representa uma resposta fornecia pelo usuário
-type Resposta struct {
-	NumeroPergunta string
-	Resposta       string
-}
 
 // NaPraticaExercicio6 é a função que exibe o enunciado do exercício 6
 func NaPraticaExercicio6() {
@@ -38,115 +23,57 @@ func NaPraticaExercicio6() {
 	format.FormatOutlineTopic(topic)
 }
 
-// CriarQuestionario retorna uma lista de perguntas da prova
-func CriarQuestionario() []Prova {
-	return []Prova{
-		{"1.", "Qual o menor elemento em um programa que expressa uma ação a ser executada?", "[1] Uma declaração (Statement) [2] Uma expressão: "},
-		{"2.", "A combinação de um ou mais valores, constantes, variáveis, operadores e funções que a linguagem interpreta e usa para produzir outro valor é?", "[1] Uma declaração (Statement) [2] Uma expressão: "},
-		{"3.", "Quais são parênteses?", "[1] () [2] {} [3] []: "},
-		{"4.", "Quais são colchetes?", "[1] () [2] {} [3] []: "},
-		{"5.", "Quais são chaves?", "[1] () [2] {} [3] []: "},
-		{"6.", "A abrangência de uma variável designa onde no código você pode acessar essa variável, e atribuir ou ler valores dela", "[1] Verdade [2] Mentira: "},
-		{"7.", "Um tipo de dados primitivo é um tipo composto, criado a partir de outros tipos básicos que ja vem de fabrica na linguagem", "[1] Verdade [2] Mentira: "},
-		{"8.", "O tipo int é um tipo de dado primitivo", "[1] Verdade [2] Mentira: "},
-		{"9.", "O tipo string é um tipo de dado composto", "[1] Verdade [2] Mentira: "},
-		{"10.", "Um tipo de dado composto permite que você crie estruturas formadas de outros tipos de dados", "[1] Verdade [2] Mentira: "},
-		{"11.", "Quando declaramos uma variável com a palavra chave var e não atribuimos nenhum valor a esta variável, o compilador designa para esta um valor padrão, chamado de valor zero.", "[1] Verdade [2] Mentira: "},
-		{"12.", "Palavras chaves servem a propositos especificos, mas fora isso podem ser usadas livremente ao longo do programa", "[1] Verdade [2] Mentira: "},
-		{"13.", "Palavra chave e palavra reservada designam coisas diferentes", "[1] Verdade [2] Mentira: "},
-		{"14.", "Uma palavra chave somente pode ser usada para seu proposito especifico e para declarar variaveis", "[1] Verdade [2] Mentira: "},
-		{"15.", "Em 2 + 2, os numeros 2 são operadores", "[1] Verdade [2] Mentira: "},
-		{"16.", "O termo package é uma palavra chave", "[1] Verdade [2] Mentira: "},
-		{"17.", "O termo variable é uma palavra chave", "[1] Verdade [2] Mentira: "},
-		{"18.", "O ponto de entrada para todos os programas é a função main(), que deve ficar dentro do package main", "[1] Verdade [2] Mentira: "},
-		{"19.", "O operador curto de declaração pode ser usado ao invés de var em todas as situações", "[1] Verdade [2] Mentira: "},
-		{"20.", "Quando vemos fmt.Println(), isto esta chamando a função Println() que pertence ao package fmt", "[1] Verdade [2] Mentira: "},
-		{"21.", "Identificador é o nome atribuido a uma variável, função ou constante", "[1] Verdade [2] Mentira: "},
-		{"22.", "Para utilizar uma função, variável ou constante de outro package utiliza-se o formato package-ponto-identificador. Por exemplo fmt.Println()", "[1] Verdade [2] Mentira: "},
-		{"23.", "Qual é o caracter que permite jogar fora um valor? Ou seja, qual caracter permite que voce diga ao programa que não vai utilzar o valor retornado por uma função?", "[1] # [2] @ [3] _ [4] $ [5] isso é pegadinha...: "},
-		{"24.", "Uma função cujo parâmetro é '... interface{}' é uma função variatica. Isso significa que você pode passar à função um número pré determinado de valores", "[1] Verdade [2] Mentira: "},
-		{"25.", "Todo valor em Go pertence também ao tipo interface vazia, representado pela notação interface{}", "[1] Verdade [2] Mentira: "},
-		{"26.", "2+3 é uma declaração/statement, não uma expressão", "[1] Verdade [2] Mentira: "},
-		{"27.", "2+3 é uma expressão não uma declaração/statemente", "[1] Verdade [2] Mentira: "},
-		{"28.", "Se eu quiser salvar o resultado de um format printing em uma variável, posso usar a função fmt.Sprintf()", "[1] Verdade [2] Mentira: "},
-		{"29.", "Em Go podemos criar nossos proprios tipos?", "[1] Verdade [2] Mentira: "},
-		{"30.", "Falando de tipos, em Go utilizamos o termo 'coerção' diferentemente de Java, por exemplo, onde se utiliza o termo 'conversão'", "[1] Verdade [2] Mentira: "},
-		{"31.", "Todo tipo criado pelo programador tem um tipo subjacente", "[1] Verdade [2] Mentira: "},
-	}
-}
-
-// ColetarRespostas coleta as respostas do usuário para as perguntas da prova
-func ColetarRespostas(perguntas []Prova) ([]Resposta, error) {
-	var listaRespostas []Resposta
-	var resposta string
-
-	// Exibe as perguntas da prova e coleta as respostas do usuário
-	for _, pergunta := range perguntas {
-		fmt.Println(pergunta.NumeroPergunta, pergunta.Perguntas)
-		fmt.Println(pergunta.Opcoes)
-		fmt.Scan(&resposta)
-
-		if !opcaoValida(resposta, pergunta.Opcoes) {
-			return nil, errors.New("opção inválida")
-		}
-
-		// Adiciona as respostas do usuário a lista de respostas concatenando o número da pergunta e a resposta
-		// gerando uma lista
-		listaRespostas = append(listaRespostas, Resposta{pergunta.NumeroPergunta, resposta})
-	}
-
-	return listaRespostas, nil
-}
-
-// opcaoValida verifica se a opção fornecida pelo usuário
-func opcaoValida(resposta, opcoes string) bool {
-	for _, opt := range strings.Fields(opcoes) {
-		opt = strings.Trim(opt, "[]")
-		if resposta == opt {
-			return true
-		}
-	}
-
-	return false
-}
-
-// ImprimirRespostas exibe as respostas fornecidas pelo usuário
-func ImprimirRespostas(respostas []Resposta) {
-	fmt.Println("Respostas da prova:")
-
-	// Exibe as respostas fornecidas pelo usuário em duas colunas de 15 perguntas com suas respectivas respostas
-	for i := 0; i < len(respostas); i++ {
-		if i+1 < len(respostas) && i+15 < len(respostas) {
-			fmt.Printf("P: %-4s R: %-8s P: %-4s R: %-8s\n", respostas[i].NumeroPergunta, respostas[i].Resposta, respostas[i+15].NumeroPergunta, respostas[i+15].Resposta)
-		}
-	}
-}
-
-// ImprimiGabarito exibe o gabarito da prova
-func ImprimiGabarito() {
-	gabarito := []Resposta{
-		{"1.", "1"}, {"2.", "2"}, {"3.", "1"}, {"4.", "3"}, {"5.", "2"}, {"6.", "1"}, {"7.", "2"}, {"8.", "1"}, {"9.", "2"}, {"10.", "1"}, {"11.", "1"},
-		{"12.", "2"}, {"13.", "1"}, {"14.", "2"}, {"15.", "2"}, {"16.", "1"}, {"17.", "2"}, {"18.", "1"}, {"19.", "2"}, {"20.", "1"}, {"21.", "1"},
-		{"22.", "1"}, {"23.", "3"}, {"24.", "1"}, {"25.", "1"}, {"26.", "2"}, {"27.", "1"}, {"28.", "1"}, {"29.", "1"}, {"30.", "1"}, {"31.", "1"},
-	}
-
-	// Exibe o gabarito da prova para o usuário em  duas colunas de 15 perguntas com suas respectivas respostas
-	fmt.Println("Gabarito da prova:")
-	for i := 0; i < len(gabarito); i++ {
-		if i+1 < len(gabarito) && i+15 < len(gabarito) {
-			fmt.Printf("P: %-4s R: %-8s P: %-4s R: %-8s\n", gabarito[i].NumeroPergunta, gabarito[i].Resposta, gabarito[i+15].NumeroPergunta, gabarito[i+15].Resposta)
-		}
-	}
-}
-
 func RespondaAProva() {
-	perguntas := CriarQuestionario()
-	respostas, err := ColetarRespostas(perguntas)
+	questionario := []format.Questionario{
+		{Numero: "1.", Pergunta: "Qual o menor elemento em um programa que expressa uma ação a ser executada?", Opcoes: "[1] Uma declaração (Statement) [2] Uma expressão: "},
+		{Numero: "2.", Pergunta: "A combinação de um ou mais valores, constantes, variáveis, operadores e funções que a linguagem interpreta e usa para produzir outro valor é?", Opcoes: "[1] Uma declaração (Statement) [2] Uma expressão: "},
+		{Numero: "3.", Pergunta: "Quais são parênteses?", Opcoes: "[1] () [2] {} [3] []: "},
+		{Numero: "4.", Pergunta: "Quais são colchetes?", Opcoes: "[1] () [2] {} [3] []: "},
+		{Numero: "5.", Pergunta: "Quais são chaves?", Opcoes: "[1] () [2] {} [3] []: "},
+		{Numero: "6.", Pergunta: "A abrangência de uma variável designa onde no código você pode acessar essa variável, e atribuir ou ler valores dela", Opcoes: "[1] Verdade [2] Mentira: "},
+		{Numero: "7.", Pergunta: "Um tipo de dados primitivo é um tipo composto, criado a partir de outros tipos básicos que ja vem de fabrica na linguagem", Opcoes: "[1] Verdade [2] Mentira: "},
+		{Numero: "8.", Pergunta: "O tipo int é um tipo de dado primitivo", Opcoes: "[1] Verdade [2] Mentira: "},
+		{Numero: "9.", Pergunta: "O tipo string é um tipo de dado composto", Opcoes: "[1] Verdade [2] Mentira: "},
+		{Numero: "10.", Pergunta: "Um tipo de dado composto permite que você crie estruturas formadas de outros tipos de dados", Opcoes: "[1] Verdade [2] Mentira: "},
+		{Numero: "11.", Pergunta: "Quando declaramos uma variável com a palavra chave var e não atribuimos nenhum valor a esta variável, o compilador designa para esta um valor padrão, chamado de valor zero.", Opcoes: "[1] Verdade [2] Mentira: "},
+		{Numero: "12.", Pergunta: "Palavras chaves servem a propositos especificos, mas fora isso podem ser usadas livremente ao longo do programa", Opcoes: "[1] Verdade [2] Mentira: "},
+		{Numero: "13.", Pergunta: "Palavra chave e palavra reservada designam coisas diferentes", Opcoes: "[1] Verdade [2] Mentira: "},
+		{Numero: "14.", Pergunta: "Uma palavra chave somente pode ser usada para seu proposito especifico e para declarar variaveis", Opcoes: "[1] Verdade [2] Mentira: "},
+		{Numero: "15.", Pergunta: "Em 2 + 2, os numeros 2 são operadores", Opcoes: "[1] Verdade [2] Mentira: "},
+		{Numero: "16.", Pergunta: "O termo package é uma palavra chave", Opcoes: "[1] Verdade [2] Mentira: "},
+		{Numero: "17.", Pergunta: "O termo variable é uma palavra chave", Opcoes: "[1] Verdade [2] Mentira: "},
+		{Numero: "18.", Pergunta: "O ponto de entrada para todos os programas é a função main(), que deve ficar dentro do package main", Opcoes: "[1] Verdade [2] Mentira: "},
+		{Numero: "19.", Pergunta: "O operador curto de declaração pode ser usado ao invés de var em todas as situações", Opcoes: "[1] Verdade [2] Mentira: "},
+		{Numero: "20.", Pergunta: "Quando vemos fmt.Println(), isto esta chamando a função Println() que pertence ao package fmt", Opcoes: "[1] Verdade [2] Mentira: "},
+		{Numero: "21.", Pergunta: "Identificador é o nome atribuido a uma variável, função ou constante", Opcoes: "[1] Verdade [2] Mentira: "},
+		{Numero: "22.", Pergunta: "Para utilizar uma função, variável ou constante de outro package utiliza-se o formato package-ponto-identificador. Por exemplo fmt.Println()", Opcoes: "[1] Verdade [2] Mentira: "},
+		{Numero: "23.", Pergunta: "Qual é o caracter que permite jogar fora um valor? Ou seja, qual caracter permite que voce diga ao programa que não vai utilzar o valor retornado por uma função?", Opcoes: "[1] # [2] @ [3] _ [4] $ [5] isso é pegadinha...: "},
+		{Numero: "24.", Pergunta: "Uma função cujo parâmetro é '... interface{}' é uma função variatica. Isso significa que você pode passar à função um número pré determinado de valores", Opcoes: "[1] Verdade [2] Mentira: "},
+		{Numero: "25.", Pergunta: "Todo valor em Go pertence também ao tipo interface vazia, representado pela notação interface{}", Opcoes: "[1] Verdade [2] Mentira: "},
+		{Numero: "26.", Pergunta: "2+3 é uma declaração/statement, não uma expressão", Opcoes: "[1] Verdade [2] Mentira: "},
+		{Numero: "27.", Pergunta: "2+3 é uma expressão não uma declaração/statemente", Opcoes: "[1] Verdade [2] Mentira: "},
+		{Numero: "28.", Pergunta: "Se eu quiser salvar o resultado de um format printing em uma variável, posso usar a função fmt.Sprintf()", Opcoes: "[1] Verdade [2] Mentira: "},
+		{Numero: "29.", Pergunta: "Em Go podemos criar nossos proprios tipos?", Opcoes: "[1] Verdade [2] Mentira: "},
+		{Numero: "30.", Pergunta: "Falando de tipos, em Go utilizamos o termo 'coerção' diferentemente de Java, por exemplo, onde se utiliza o termo 'conversão'", Opcoes: "[1] Verdade [2] Mentira: "},
+		{Numero: "31.", Pergunta: "Todo tipo criado pelo programador tem um tipo subjacente", Opcoes: "[1] Verdade [2] Mentira: "},
+	}
+
+	gabarito := []format.Resposta{
+		{Numero: "1.", Resposta: "1"}, {Numero: "2.", Resposta: "2"}, {Numero: "3.", Resposta: "1"}, {Numero: "4.", Resposta: "3"}, {Numero: "5.", Resposta: "2"},
+		{Numero: "6.", Resposta: "1"}, {Numero: "7.", Resposta: "2"}, {Numero: "8.", Resposta: "1"}, {Numero: "9.", Resposta: "2"}, {Numero: "10.", Resposta: "1"},
+		{Numero: "11.", Resposta: "1"}, {Numero: "12.", Resposta: "2"}, {Numero: "13.", Resposta: "1"}, {Numero: "14.", Resposta: "2"}, {Numero: "15.", Resposta: "2"},
+		{Numero: "16.", Resposta: "1"}, {Numero: "17.", Resposta: "2"}, {Numero: "18.", Resposta: "1"}, {Numero: "19.", Resposta: "2"}, {Numero: "20.", Resposta: "1"},
+		{Numero: "21.", Resposta: "1"}, {Numero: "22.", Resposta: "1"}, {Numero: "23.", Resposta: "3"}, {Numero: "24.", Resposta: "1"}, {Numero: "25.", Resposta: "1"},
+		{Numero: "26.", Resposta: "2"}, {Numero: "27.", Resposta: "1"}, {Numero: "28.", Resposta: "1"}, {Numero: "29.", Resposta: "1"}, {Numero: "30.", Resposta: "1"}, {Numero: "31.", Resposta: "1"},
+	}
+
+	perguntas := format.CriarQuestionario(questionario)
+	respostas, err := format.ColetarRespostas(perguntas)
 	if err != nil {
 		fmt.Println("Erro ao coletar respostas:", err)
 		return
 	}
 
-	ImprimirRespostas(respostas)
-	ImprimiGabarito()
+	format.ImprimirRespostas(respostas)
+	format.ImprimiGabarito(gabarito)
 }
