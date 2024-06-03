1f512723c49e1edb0714a732e8d46de269bae348
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-06-03 16:20:56 -0300

    doc: added new comments to documentation the functions and actions

--- outline/format/prova.go
+++ outline/format/prova.go
@@ -1,3 +1,4 @@
+// Pacote com funções para criar um questionário, coletar respostas e validar as respostas
 package outline
 
 import (
@@ -20,9 +21,12 @@ type Resposta struct {
 	Status   string
 }
 
+// CriarQuestionario cria um questionário com as perguntas e opções de respostas de uma prova
+// Recebe um slice de Questionario e retorna um slice de Questionario
 func CriarQuestionario(questionario []Questionario) []Questionario {
 	var prova []Questionario
 
+	// Itera sobre o slice de Questionario e adiciona as perguntas e opções de respostas
 	for _, quest := range questionario {
 		prova = append(prova, Questionario{Numero: quest.Numero, Pergunta: quest.Pergunta, Opcoes: quest.Opcoes})
 	}
@@ -30,16 +34,21 @@ func CriarQuestionario(questionario []Questionario) []Questionario {
 	return prova
 }
 
+// ColetarRespostas coleta as respostas de um questionário de uma prova e retorna um slice de Resposta
+// Recebe um slice de Questionario e retorna um slice de Resposta e um erro
 func ColetarRespostas(questionario []Questionario) ([]Resposta, error) {
 	var listaRespostas []Resposta
 	var resposta string
 
 	criar_questionario := CriarQuestionario(questionario)
 
+	// Itera sobre o slice de Questionario e coleta as respostas do usuário para cada pergunta
+	// Verifica se a resposta é válida e adiciona ao slice de Resposta ou retorna um erro
 	for _, p := range criar_questionario {
 		fmt.Print(p.Numero, p.Pergunta, p.Opcoes)
 		fmt.Scan(&resposta)
 
+		// Verifica se a resposta é válida
 		if !opcaoValida(resposta, p.Opcoes) {
 			return nil, errors.New("opção inválida")
 		}
@@ -50,7 +59,11 @@ func ColetarRespostas(questionario []Questionario) ([]Resposta, error) {
 	return listaRespostas, nil
 }
 
+// opcaoValida verifica se a resposta do usuário é válida para a pergunta da prova e retorna um boolean
+// Recebe a resposta e as opções de respostas da pergunta e retorna um boolean
 func opcaoValida(resposta, opcoes string) bool {
+	// Itera sobre as opções de respostas da pergunta e verifica se a resposta do usuário é válida
+	// Retorna true se a resposta for válida e false se for inválida
 	for _, opt := range strings.Fields(opcoes) {
 		opt = strings.Trim(opt, "[]")
 		if resposta == opt {
@@ -61,10 +74,13 @@ func opcaoValida(resposta, opcoes string) bool {
 	return false
 }
 
+// ValidarRespostas valida as respostas do usuário com o gabarito da prova e exibe o resultado da prova na tela
+// Recebe um slice de Resposta e um slice de Resposta
 func ValidarRespostas(respostas []Resposta, gabarito []Resposta) {
 	var validaRespostas []Resposta
 	var totalCorretas int
 
+	// Itera sobre as respostas do usuário e compara com o gabarito da prova
 	for i, resp := range respostas {
 		if resp.Resposta == gabarito[i].Resposta {
 			validaRespostas = append(validaRespostas, Resposta{resp.Numero, resp.Resposta, "Correta"})
@@ -74,12 +90,15 @@ func ValidarRespostas(respostas []Resposta, gabarito []Resposta) {
 		}
 	}
 
+	// Exibe o resultado da prova na tela
 	fmt.Printf("\nResultado da prova:\n")
 	fmt.Printf("\nTotal de respostas corretas: %d de %d\n", totalCorretas, len(gabarito))
 
+	// Calcula o percentual de acerto do usuário na prova
 	resultadoDaProva := float64(totalCorretas) / float64(len(gabarito)) * 100
 	fmt.Printf("Percentual de acerto: %.2f%%.\n\n", resultadoDaProva)
 
+	// Exibe as respostas do usuário e o status de cada resposta
 	for _, v := range validaRespostas {
 		fmt.Printf("P: %-4s R: %-4s %s\n", v.Numero, v.Resposta, v.Status)
 	}
