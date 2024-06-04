a9a18a9fe585bec9d2f56e666c6016fa46e00d9e
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-06-04 19:09:35 -0300

    feat: Add Loops: break e continue to outline menu

---
+++ outline/fluxo_de_controle/loops_break_e_continue.go
@@ -0,0 +1,17 @@
+package outline
+
+import format "github.com/fabianoflorentino/aprendago/outline/format"
+
+func LoopsBreakEContinue() {
+	topic := format.OutlineContent{
+		Title: "Loops: break e continue",
+		Content: `
+- Operação módulo: %
+- For: break
+- For: continue
+- Go Playground: https://play.golang.org/p/gpKMP1wAEM & https://play.golang.org/p/8erMGEbZQix
+    `,
+	}
+
+	format.FormatOutlineTopic(topic)
+}
