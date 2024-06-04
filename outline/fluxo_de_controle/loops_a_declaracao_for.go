687b8bedba349841718ef533db29bdc9eaeeac6a
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-06-04 19:09:23 -0300

    feat: Add Loops: A declaração for to outline menu

---
+++ outline/fluxo_de_controle/loops_a_declaracao_for.go
@@ -0,0 +1,19 @@
+package outline
+
+import format "github.com/fabianoflorentino/aprendago/outline/format"
+
+func LoopsADeclaracaoFor() {
+	topic := format.OutlineContent{
+		Title: "Loops: A declaração for",
+		Content: `
+- For: inicialização, condição, pós
+- For: condição ("while")
+- For: ...ever? (http servers)
+- For: break
+- https://golang.org/ref/spec#For_statements, Effective Go
+- (Range vem mais pra frente.)
+    `,
+	}
+
+	format.FormatOutlineTopic(topic)
+}
