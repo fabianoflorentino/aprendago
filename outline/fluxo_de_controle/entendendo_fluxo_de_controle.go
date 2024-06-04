ab67f200d142f62ce6bf5579a030d267b3f89b26
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-06-04 18:45:49 -0300

    fix: added forgot content

--- outline/fluxo_de_controle/entendendo_fluxo_de_controle.go
+++ outline/fluxo_de_controle/entendendo_fluxo_de_controle.go
@@ -14,6 +14,31 @@ func EntendendoFluxoDeControle() {
   - Uma delas é o fluxo de controle de repetição (loop). Nesse caso, o computador vai repetir a leitura de um mesmo código de uma maneira específica. O fluxo de controle de repetição tambem é conhecido como fluxo de controle iterativo.
   - E o outro é o fluxo de controle condicional, ou fluxo de controle de seleção. Nesse caso o computador encontra uma condição e, através de uma declaração if ou switch, toma um curso ou outro dependendo dessa condição.
 - Ou seja, há três tipos de fluxo de controle: sequencial, de repetição e condicional.
+- Nesse capítulo:
+    - Sequencial
+    - Iterativo (loop)
+      - for: inicialização, condição, pós
+      - for: hierarquicamente
+      - for: condição ("while")
+      - for: ...ever?
+      - for: break
+      - for: continue
+    - Condicional
+      - declarações switch/case/default
+        - não há fall-through por padrão
+        - criando fall-through
+        - default
+        - múltiplos casos
+        - casos podem ser expressões
+            - se resultarem em true, rodam
+        - tipo
+        - if
+          - bool
+          - o operador "!"
+          - declaração de inicialização
+          - if, else
+          - if, else if, else
+          - if, else if, else if, ..., else
     `,
 	}
 
