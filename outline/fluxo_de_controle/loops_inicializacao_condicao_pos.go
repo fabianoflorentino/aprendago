9e6af2196bc69dfd745b09756e6046f5362f85a2
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-06-04 18:50:39 -0300

    feat: Add Loops Inicialização, Condição e Pós to outline menu

---
+++ outline/fluxo_de_controle/loops_inicializacao_condicao_pos.go
@@ -0,0 +1,18 @@
+package outline
+
+import format "github.com/fabianoflorentino/aprendago/outline/format"
+
+func LoopsInicializacaoCondicaoPos() {
+	topic := format.OutlineContent{
+		Title: "Loops: Inicialização, Condição e Pós",
+		Content: `
+- For
+  - Inicialização, condição, pós
+  - Ponto e vírgula?
+  - https://gobyexample.com
+  - Não existe while!
+    `,
+	}
+
+	format.FormatOutlineTopic(topic)
+}
