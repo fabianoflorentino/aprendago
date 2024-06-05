6b7827236fb705b1ea132743a432592fa132c386
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-06-05 11:32:13 -0300

    feat: Add Condicionais: A declaração if to outline menu

---
+++ outline/fluxo_de_controle/condicionais_a_declaracao_if.go
@@ -0,0 +1,17 @@
+package outline
+
+import format "github.com/fabianoflorentino/aprendago/outline/format"
+
+func CondicionaisADeclaracaoIf() {
+	topic := format.OutlineContent{
+		Title: "Condicionais: A declaração if",
+		Content: `
+- If: bool
+- If: o operador não → "!"
+- If: declaração de inicialização
+- Go Playground: https://play.golang.org/p/6nq2Tjb07i
+    `,
+	}
+
+	format.FormatOutlineTopic(topic)
+}
