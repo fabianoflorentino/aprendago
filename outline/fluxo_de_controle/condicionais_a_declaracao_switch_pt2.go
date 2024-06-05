3672d76499907a22db18c4658443a4ad0cf9ee0d
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-06-05 11:32:49 -0300

    feat: Add Condicionais: A declaração switch pt. 2 & documentação

---
+++ outline/fluxo_de_controle/condicionais_a_declaracao_switch_pt2.go
@@ -0,0 +1,15 @@
+package outline
+
+import format "github.com/fabianoflorentino/aprendago/outline/format"
+
+func CondicionaisADeclaracaoSwitchPt2() {
+	topic := format.OutlineContent{
+		Title: "Condicionais: A declaração switch pt. 2 & documentação",
+		Content: `
+- Pode avaliar tipos
+- Pode haver uma expressão de inicialização
+    `,
+	}
+
+	format.FormatOutlineTopic(topic)
+}
