29d006d48235687a6d9b271bf8d2a15a13f3099b
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-06-05 11:32:42 -0300

    feat: Add Condicionais: A declaração switch to outline menu

---
+++ outline/fluxo_de_controle/condicionais_a_declaracao_switch.go
@@ -0,0 +1,21 @@
+package outline
+
+import format "github.com/fabianoflorentino/aprendago/outline/format"
+
+func CondicionaisADeclaracaoSwitch() {
+	topic := format.OutlineContent{
+		Title: "Condicionais: A declaração switch",
+		Content: `
+- Switch:
+- pode avaliar uma expressão
+  - switch statement == case (value)
+  - default switch statement == true (bool)
+- não há fall-through por padrão
+- criando fall-through
+- default
+- cases compostos
+    `,
+	}
+
+	format.FormatOutlineTopic(topic)
+}
