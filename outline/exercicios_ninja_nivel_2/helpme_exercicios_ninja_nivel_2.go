711b388ad942b6d1a762c640149701bce46e1a24
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-06-06 10:34:02 -0300

    feat: Add HelpMe functionality to Exercicios Ninja Nivel 2 outline menu

---
+++ outline/exercicios_ninja_nivel_2/helpme_exercicios_ninja_nivel_2.go
@@ -0,0 +1,29 @@
+package outline
+
+import (
+	"fmt"
+
+	helpme "github.com/fabianoflorentino/aprendago/outline/format"
+)
+
+func HelpMeExerciciosNinjaNivel2() {
+	hlp := []helpme.HelpMe{
+		{Flag: "--na-pratica-exercicio-1 --nivel-2", Description: "Apresenta o primeiro exercício prático do nível 2.", Width: 0},
+		{Flag: "--na-pratica-exercicio-1 --nivel-2 --resolucao", Description: "Exibe a resolução do primeiro exercício prático do nível 2.", Width: 0},
+		{Flag: "--na-pratica-exercicio-2 --nivel-2", Description: "Apresenta o segundo exercício prático do nível 2.", Width: 0},
+		{Flag: "--na-pratica-exercicio-2 --nivel-2 --resolucao", Description: "Exibe a resolução do segundo exercício prático do nível 2.", Width: 0},
+		{Flag: "--na-pratica-exercicio-3 --nivel-2", Description: "Apresenta o terceiro exercício prático do nível 2.", Width: 0},
+		{Flag: "--na-pratica-exercicio-3 --nivel-2 --resolucao", Description: "Exibe a resolução do terceiro exercício prático do nível 2.", Width: 0},
+		{Flag: "--na-pratica-exercicio-4 --nivel-2", Description: "Apresenta o quarto exercício prático do nível 2.", Width: 0},
+		{Flag: "--na-pratica-exercicio-4 --nivel-2 --resolucao", Description: "Exibe a resolução do quarto exercício prático do nível 2.", Width: 0},
+		{Flag: "--na-pratica-exercicio-5 --nivel-2", Description: "Apresenta o quinto exercício prático do nível 2.", Width: 0},
+		{Flag: "--na-pratica-exercicio-5 --nivel-2 --resolucao", Description: "Exibe a resolução do quinto exercício prático do nível 2.", Width: 0},
+		{Flag: "--na-pratica-exercicio-6 --nivel-2", Description: "Apresenta o sexto exercício prático do nível 2.", Width: 0},
+		{Flag: "--na-pratica-exercicio-6 --nivel-2 --resolucao", Description: "Exibe a resolução do sexto exercício prático do nível 2.", Width: 0},
+		{Flag: "--na-pratica-exercicio-7 --nivel-2", Description: "Apresenta o sétimo exercício prático do nível 2.", Width: 0},
+		{Flag: "--na-pratica-exercicio-7 --nivel-2 --prova", Description: "Exibe a prova do sétimo exercício prático do nível 2.", Width: 0},
+	}
+
+	fmt.Println("\nCapítulo 5: Exercícios Ninja - Nível 2")
+	helpme.PrintHelpMe(hlp)
+}
