17d268f5fc8736d8f5f85da3f77c23ecfe3ba302
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-06-06 11:26:31 -0300

    feat: Add NaPraticaExercicio1 level 3 function to outline package

---
+++ outline/exercicios_ninja_nivel_3/na_pratica_exercicio_1.go
@@ -0,0 +1,25 @@
+package outline
+
+import (
+	"fmt"
+
+	format "github.com/fabianoflorentino/aprendago/outline/format"
+)
+
+func NaPraticaExercicio1() {
+  topic := format.OutlineContent{
+    Title: "Na Prática - Exercício #1",
+    Content: `
+- Põe na tela: todos os números de 1 a 10000.
+- Solução: https://play.golang.org/p/MkdZiDW8SQ
+    `,
+  }
+
+  format.FormatOutlineTopic(topic)
+}
+
+func ResolucaoNaPraticaExercicio1() {
+  for i := 1; i <= 60; i++ {
+    fmt.Printf("%d ", i)
+  }
+}
