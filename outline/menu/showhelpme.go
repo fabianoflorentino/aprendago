3212285902baa85f282fd5f7e69fac8e542cfd28
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-06-06 10:34:14 -0300

    feat: Add HelpMe functionality to Fluxo de Controle, Exercicios Ninja Nivel 2, and Fundamentos da Programacao outline menus

--- outline/menu/showhelpme.go
+++ outline/menu/showhelpme.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 
 	exercicios_ninja_nivel_1 "github.com/fabianoflorentino/aprendago/outline/exercicios_ninja_nivel_1"
+	exercicios_ninja_nivel_2 "github.com/fabianoflorentino/aprendago/outline/exercicios_ninja_nivel_2"
+	fluxo_de_controle "github.com/fabianoflorentino/aprendago/outline/fluxo_de_controle"
+	fumdamentos_da_programacao "github.com/fabianoflorentino/aprendago/outline/fundamentos_da_programacao"
 	variaveis_valores_tipos "github.com/fabianoflorentino/aprendago/outline/variaveis_valores_tipos"
 	visao_geral_do_curso "github.com/fabianoflorentino/aprendago/outline/visao_geral_do_curso"
 )
@@ -14,12 +17,20 @@ Uso: go run main.go [opção]
 
 Exemplo:
 	go run main.go --bem-vindo
+
+Ajuda:
+
+--outline  Exibe o outline completo do curso.
+--help     Exibe a lista de todas as opções disponíveis.
 	`
 
 	fmt.Println(header)
 	visao_geral_do_curso.HelpMeVisaoGeralDoCurso()
 	variaveis_valores_tipos.HelpMeVariaveisValoresTipos()
 	exercicios_ninja_nivel_1.HelpMeExerciciosNinjaNivel1()
+	fumdamentos_da_programacao.HelpMeFundamentosDaProgramacao()
+	exercicios_ninja_nivel_2.HelpMeExerciciosNinjaNivel2()
+	fluxo_de_controle.HelpMeFluxoDeControle()
 }
 
 // import (
