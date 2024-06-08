07395e266a5b21c39f9807aa47e5771826ebc101
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-06-08 18:36:54 -0300

    feat: Add NaPraticaExercicio8 function to outline package

---
+++ outline/exercicios_ninja_nivel_3/na_pratica_exercicio_8.go
@@ -0,0 +1,30 @@
+package outline
+
+import (
+	"fmt"
+
+	format "github.com/fabianoflorentino/aprendago/outline/format"
+)
+
+func NaPraticaExercicio8() {
+	topic := format.OutlineContent{
+		Title: "Na prática: Exercício #8",
+		Content: `
+- Crie um programa que utilize a declaração switch, sem switch statement especificado.
+- Solução: https://play.golang.org/p/TyIGp4Hi8B
+    `,
+	}
+
+	format.FormatOutlineTopic(topic)
+}
+
+func ResolucaoNaPraticaExercicio8() {
+	statement := true
+
+	switch {
+	case statement:
+		fmt.Println("Não deve ser impresso")
+	case !statement:
+		fmt.Println("Deve ser impresso")
+	}
+}
