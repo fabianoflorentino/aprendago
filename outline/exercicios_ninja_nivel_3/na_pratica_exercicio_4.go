c980dd88e87817b5cbb2877d2cc86da5a7bdbc99
Fabiano Santos Florentino <florentino@kyndryl.com>
2024-06-08 11:30:36 -0300

    feat: Add solution for NaPraticaExercicio4 in outline package

---
+++ outline/exercicios_ninja_nivel_3/na_pratica_exercicio_4.go
@@ -0,0 +1,40 @@
+package outline
+
+import (
+	"fmt"
+	"time"
+
+	format "github.com/fabianoflorentino/aprendago/outline/format"
+)
+
+func NaPraticaExercicio4() {
+	topic := format.OutlineContent{
+		Title: "Na prática: Exercício #4",
+		Content: `
+- Crie um loop utilizando a sintaxe: for {}
+- Utilize-o para demonstrar os anos desde que você nasceu.
+- Solução: https://play.golang.org/p/dIbfdiuw0ms
+    `,
+	}
+
+	format.FormatOutlineTopic(topic)
+}
+
+func ResolucaoNaPraticaExercicio4() {
+	var birdthYear int = 1985
+	currentYear := time.Now().Year()
+
+	// Nesse formato de loop, é importante quebrar o loop com o break
+	for {
+		if birdthYear >= currentYear {
+			fmt.Printf("%d ", birdthYear)
+			// nessário para quebrar o loop
+			break
+		} else {
+			fmt.Printf("%d, ", birdthYear)
+			birdthYear++
+		}
+	}
+
+	fmt.Printf("\nSua idade: %v", time.Now().Year()-1985)
+}
