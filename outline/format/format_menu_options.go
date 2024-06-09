99cf420d386011257b0a10ceb0266a824cb7ef76
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-06-09 12:28:42 -0300

    refact: Simplify FormatMenuOptions function in outline package

--- outline/format/format_menu_options.go
+++ outline/format/format_menu_options.go
@@ -25,29 +25,11 @@ func FormatMenuOptions(args []string, options []MenuOptions) {
 
 	// Verifica se a opção passada está contida nas opções disponíveis
 	for _, opt := range options {
-		// Separa as opções em um slice de strings
-		optComponents := strings.Fields(opt.Options)
-
-		// Verifica se a opção passada contém todas as opções disponíveis
-		if contains(argStr, optComponents) {
+		if argStr == opt.Options {
 			opt.ExecFunc()
 			return
 		}
 	}
 
 	fmt.Print(headerMenu)
-}
-
-// contains verifica se a string passada contém todas as opções disponíveis
-func contains(argStr string, options []string) bool {
-	// Verifica se a string passada contém todas as opções disponíveis
-	for _, opt := range options {
-		// Se a string não contém a opção, retorna falso
-		if !strings.Contains(argStr, opt) {
-			return false
-		}
-	}
-
-	// Se a string contém todas as opções, retorna verdadeiro
-	return true
 }
