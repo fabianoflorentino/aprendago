302072c5ea278e6e28bff57786b492e48af7f5f6
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-06-09 14:07:30 -0300

    feat: Add menu options for FundamentosDaProgramcao in outline package

---
+++ outline/fundamentos_da_programacao/menu_fundamentos_da_programcao.go
@@ -0,0 +1,17 @@
+package outline
+
+import format "github.com/fabianoflorentino/aprendago/outline/format"
+
+func MenuFundamentosDaProgramcao([]string) []format.MenuOptions {
+	return []format.MenuOptions{
+		{Options: "--tipo-booleano", ExecFunc: func() { TipoBooleano() }},
+		{Options: "--como-os-computadores-funcionam", ExecFunc: func() { ComoOsComputadoresFuncionam() }},
+		{Options: "--tipos-numericos", ExecFunc: func() { TiposNumericos() }},
+		{Options: "--overflow", ExecFunc: func() { Overflow() }},
+		{Options: "--tipo-string", ExecFunc: func() { TipoString() }},
+		{Options: "--sistemas-numericos", ExecFunc: func() { SistemasNumericos() }},
+		{Options: "--constantes", ExecFunc: func() { Constantes() }},
+		{Options: "--iota", ExecFunc: func() { Iota() }},
+		{Options: "--deslocamento-de-bits", ExecFunc: func() { DeslocamentoDeBits() }},
+	}
+}
