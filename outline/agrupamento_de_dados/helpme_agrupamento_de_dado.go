bec71c59288b2bd6e1ec72e5358ec151b95b3592
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-06-09 20:45:33 -0300

    feat: Add HelpMeAgrupamentoDeDados function to outline package

---
+++ outline/agrupamento_de_dados/helpme_agrupamento_de_dado.go
@@ -0,0 +1,16 @@
+package outline
+
+import (
+	"fmt"
+
+	helpme "github.com/fabianoflorentino/aprendago/outline/format"
+)
+
+func HelpMeAgrupamentoDeDados() {
+	hlp := []helpme.HelpMe{
+		{Flag: "--array", Description: "Apresenta o tópico Array.", Width: 0},
+	}
+
+	fmt.Println("\nCapítulo 8: Agrupamento de Dados")
+	helpme.PrintHelpMe(hlp)
+}
