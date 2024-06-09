66835027fe156d60fb7e4c0bc047f7a581823793
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-06-09 16:14:45 -0300

    feat: Add menu options for ExerciciosNinjaNivel3 and FluxoDeControle in outline package

--- outline/menu/menu.go
+++ outline/menu/menu.go
@@ -6,6 +6,8 @@ import (
 	outline "github.com/fabianoflorentino/aprendago/outline"
 	exercicios_ninja_nivel_1 "github.com/fabianoflorentino/aprendago/outline/exercicios_ninja_nivel_1"
 	exercicios_ninja_nivel_2 "github.com/fabianoflorentino/aprendago/outline/exercicios_ninja_nivel_2"
+	exercicios_ninja_nivel_3 "github.com/fabianoflorentino/aprendago/outline/exercicios_ninja_nivel_3"
+	fluxo_de_controle "github.com/fabianoflorentino/aprendago/outline/fluxo_de_controle"
 	format "github.com/fabianoflorentino/aprendago/outline/format"
 	fundamentos_da_programacao "github.com/fabianoflorentino/aprendago/outline/fundamentos_da_programacao"
 	variaveis_valores_tipos "github.com/fabianoflorentino/aprendago/outline/variaveis_valores_tipos"
@@ -23,6 +25,8 @@ func Menu(args []string) {
 		exercicios_ninja_nivel_1.MenuExerciciosNinjaNivel1(args),
 		fundamentos_da_programacao.MenuFundamentosDaProgramcao(args),
 		exercicios_ninja_nivel_2.MenuExerciciosNinjaNivel2(args),
+		fluxo_de_controle.MenuFluxoDeControle(args),
+		exercicios_ninja_nivel_3.MenuExerciciosNinjaNivel3(args),
 	)
 }
 
