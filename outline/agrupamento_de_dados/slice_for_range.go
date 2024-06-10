1187b0f489e16751ea937c0be84be56a0c8e01eb
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-06-10 19:19:29 -0300

    feat: Add SliceForRange function to handle looping through a slice using the range keyword

---
+++ outline/agrupamento_de_dados/slice_for_range.go
@@ -0,0 +1,22 @@
+package agrupamento_de_dados
+
+import "github.com/fabianoflorentino/aprendago/outline/format"
+
+func SliceForRange() {
+	topic := format.OutlineContent{
+		Title: "Slice: for range",
+		Content: `
+- Slices:
+  - Tamanho: len(x)
+  - Índice específico: x[i] (0-based)
+- Para ver todos os itens de uma slice utilizamos o loop for com range.
+- Range significa alcance, faixa, extensão.
+- For range: for i, v := range x {}
+- Go Playground:
+  - https://play.golang.org/p/h5-RFJn-Fh
+  - https://play.golang.org/p/2wj02m3-eM
+    `,
+	}
+
+	format.FormatOutlineTopic(topic)
+}
