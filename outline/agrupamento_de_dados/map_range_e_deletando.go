ac81c1efc75456560cea0cbc9bcd53a2393dfec9
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-06-14 14:20:11 -0300

    feat: Add MapRangeEDeletando function to handle map range and deleting

---
+++ outline/agrupamento_de_dados/map_range_e_deletando.go
@@ -0,0 +1,19 @@
+package agrupamento_de_dados
+
+import "github.com/fabianoflorentino/aprendago/outline/format"
+
+func MapRangeEDeletando() {
+	topic := format.OutlineContent{
+		Title: "Map Range e Deletando",
+		Content: `
+- Range: for k, v := range map { }
+- Reiterando: maps *não tem ordem* e um range usará uma ordem aleatória.
+- Go Playground: https://play.golang.org/p/6zEMfIP-AE
+- delete(map, key)
+- Deletar uma key não-existente não retorna erros!
+- Go Playground: https://play.golang.org/p/0uuIicU3Zz
+    `,
+	}
+
+	format.FormatOutlineTopic(topic)
+}
