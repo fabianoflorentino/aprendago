b034687c57e8175a11b886f3a60c791c3a6a5d03
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-06-14 15:46:30 -0300

    feat: Add ExerciciosNinjaNivel4 function to handle Ninja Level 4 exercises

---
+++ outline/exercicios_ninja_nivel_4/topics.go
@@ -0,0 +1,10 @@
+package exercicios_ninja_nivel_4
+
+import "fmt"
+
+func ExerciciosNinjaNivel4() {
+	fmt.Printf("09 - Exercícios Ninja Nível 4\n\n")
+
+	NaPraticaExercicio1()
+	ResolucaoNaPraticaExercicio1()
+}
