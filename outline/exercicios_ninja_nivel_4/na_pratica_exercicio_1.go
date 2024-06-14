fba3dc5b3b00a48ecbc1a1748a77f17d70ccaa8f
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-06-14 15:45:56 -0300

    feat: Add NaPraticaExercicio1 function to handle exercise #1 in the practical section

---
+++ outline/exercicios_ninja_nivel_4/na_pratica_exercicio_1.go
@@ -0,0 +1,28 @@
+package exercicios_ninja_nivel_4
+
+import (
+	"fmt"
+
+	"github.com/fabianoflorentino/aprendago/outline/format"
+)
+
+func NaPraticaExercicio1() {
+	topic := format.OutlineContent{
+		Title: "Na pratica: Exercício #1",
+		Content: `
+- Usando uma literal composta:
+  - Crie um array que suporte 5 valores to tipo int
+  - Atribua valores aos seus índices
+- Utilize range e demonstre os valores do array.
+- Utilizando format printing, demonstre o tipo do array.
+- Solução: https://play.golang.org/p/tpWDzzlO2l
+    `,
+	}
+
+	format.FormatOutlineTopic(topic)
+}
+
+func ResolucaoNaPraticaExercicio1() {
+	array := [5]int{1, 2, 3, 4, 5}
+	fmt.Printf("Tipo: %T", array)
+}
