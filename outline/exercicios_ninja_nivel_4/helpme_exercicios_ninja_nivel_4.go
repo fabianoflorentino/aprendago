51b48d7cccc3e0e04efaee38733d608bf4690fa4
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-06-14 15:46:11 -0300

    feat: Add HelpMeExerciciosNinjaNivel4 function to handle exercises in Ninja Level 4

---
+++ outline/exercicios_ninja_nivel_4/helpme_exercicios_ninja_nivel_4.go
@@ -0,0 +1,17 @@
+package exercicios_ninja_nivel_4
+
+import (
+	"fmt"
+
+	"github.com/fabianoflorentino/aprendago/outline/format"
+)
+
+func HelpMeExerciciosNinjaNivel4() {
+	hlp := []format.HelpMe{
+		{Flag: "--na-pratica-exercicio-1 --nivel-4", Description: "Apresenta o primeiro exercício prático do curso.", Width: 0},
+		{Flag: "--na-pratica-exercicio-1 --nivel-4 --resolucao", Description: "Exibe a resolução do primeiro exercício prático.", Width: 0},
+	}
+
+	fmt.Println("\n09 - Exercícios Ninja Nível 4")
+	format.PrintHelpMe(hlp)
+}
