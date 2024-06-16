887264d6e78176b745e27e302c30d4df06ba76bf
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-06-16 11:44:07 -0300

    feat: Add solution for exercise 5 in "Na pratica" section

---
+++ outline/exercicios_ninja_nivel_4/na_pratica_exercicio_5.go
@@ -0,0 +1,29 @@
+package exercicios_ninja_nivel_4
+
+import (
+	"fmt"
+
+	"github.com/fabianoflorentino/aprendago/outline/format"
+)
+
+func NaPraticaExercicio5() {
+	topic := format.OutlineContent{
+		Title: "Na pratica: Exercício #5",
+		Content: `
+- Comece com essa slice:
+    - x := []int{42, 43, 44, 45, 46, 47, 48, 49, 50, 51}
+- Utilizando slicing e append, crie uma slice y que contenha os valores:
+    - [42, 43, 44, 48, 49, 50, 51]
+- Solução: https://play.golang.org/p/26bT-UKmJH
+    `,
+	}
+
+	format.FormatOutlineTopic(topic)
+}
+
+func ResolucaoNaPraticaExercicio5() {
+	slice := []int{42, 43, 44, 45, 46, 47, 48, 49, 50, 51}
+	novoSlice := append(slice[:3], slice[6:]...)
+
+	fmt.Printf("%v", novoSlice)
+}
