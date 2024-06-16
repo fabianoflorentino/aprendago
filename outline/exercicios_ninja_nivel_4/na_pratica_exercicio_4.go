37f032286375c897dd093e61272e59decf925429
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-06-16 11:26:21 -0300

    chore: Add solution for exercise 4 in "Na pratica" section

---
+++ outline/exercicios_ninja_nivel_4/na_pratica_exercicio_4.go
@@ -0,0 +1,41 @@
+package exercicios_ninja_nivel_4
+
+import (
+	"fmt"
+
+	"github.com/fabianoflorentino/aprendago/outline/format"
+)
+
+func NaPraticaExercicio4() {
+	topic := format.OutlineContent{
+		Title: "Na pratica: Exercício #4",
+		Content: `
+- Começando com a seguinte slice:
+    - x := []int{42, 43, 44, 45, 46, 47, 48, 49, 50, 51}
+- Anexe a ela o valor 52;
+- Anexe a ela os valores 53, 54 e 55 utilizando uma única declaração;
+- Demonstre a slice;
+- Anexe a ela a seguinte slice:
+    - y := []int{56, 57, 58, 59, 60}
+- Demonstre a slice x.
+- Solução: https://play.golang.org/p/6WNJ0Otpy0
+    `,
+	}
+
+	format.FormatOutlineTopic(topic)
+}
+
+func ResolucaoNaPraticaExercicio4() {
+	slice := []int{42, 43, 44, 45, 46, 47, 48, 49, 50, 51}
+	append52 := append(slice, 52)
+
+	append53to55 := append(append52, 53, 54, 55)
+
+	fmt.Printf("Slice: %v\n", append53to55)
+
+	sliceY := []int{56, 57, 58, 59, 60}
+
+	appendSliceY := append(append53to55, sliceY...)
+
+	fmt.Printf("Slice: %v\n", appendSliceY)
+}
