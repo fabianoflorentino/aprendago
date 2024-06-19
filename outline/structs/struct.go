7a170c29447a45d429a722e170e69df343ef0fcb
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-06-18 22:13:09 -0300

    feat: add Structs topic to outline

---
+++ outline/structs/struct.go
@@ -0,0 +1,19 @@
+package structs
+
+import "github.com/fabianoflorentino/aprendago/outline/format"
+
+func Struct() {
+	topic := format.OutlineContent{
+		Title: "10 - Structs",
+		Content: `
+- Struct é um tipo de dados composto que nos permite armazenar valores de tipos *diferentes.*
+- Seu nome vem de "structure," ou estrutura.
+- Declaração: type x struct { y: z }
+- Acesso: x.y
+- Exemplo: nome, idade, fumante.
+- Go Playground: https://play.golang.org/p/5i0DqxuBp1
+    `,
+	}
+
+	format.FormatOutlineTopic(topic)
+}
