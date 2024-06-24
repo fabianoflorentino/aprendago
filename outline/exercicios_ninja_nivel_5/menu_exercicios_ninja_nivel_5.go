464bb8616e51f089326327de5f6953e91dddbcea
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-06-24 10:07:42 -0300

    feat: Add NaPraticaExercicio2 and menu option for it in exercicios_ninja_nivel_5 package

--- outline/exercicios_ninja_nivel_5/menu_exercicios_ninja_nivel_5.go
+++ outline/exercicios_ninja_nivel_5/menu_exercicios_ninja_nivel_5.go
@@ -6,5 +6,7 @@ func MenuExerciciosNinjaNivel5([]string) []format.MenuOptions {
 	return []format.MenuOptions{
 		{Options: "--na-pratica-exercicio-1 --nivel-5", ExecFunc: func() { NaPraticaExercicio1() }},
 		{Options: "--na-pratica-exercicio-1 --nivel-5 --resolucao", ExecFunc: func() { ResolucaoNaPraticaExercicio1() }},
+		{Options: "--na-pratica-exercicio-2 --nivel-5", ExecFunc: func() { NaPraticaExercicio2() }},
+		{Options: "--na-pratica-exercicio-2 --nivel-5 --resolucao", ExecFunc: func() { ResolucaoNaPraticaExercicio2() }},
 	}
 }
--- outline/exercicios_ninja_nivel_5/topics.go
+++ outline/exercicios_ninja_nivel_5/topics.go
@@ -6,4 +6,5 @@ func ExerciciosNinjaNivel5() {
 	fmt.Printf("Capítulo 11: Exercícios Ninja Nível 5\n\n")
 
 	NaPraticaExercicio1()
+	NaPraticaExercicio2()
 }
