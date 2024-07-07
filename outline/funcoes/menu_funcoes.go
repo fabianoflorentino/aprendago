7dbe1becc139cb74f5f542229afa54671ce53c58
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-07-07 13:18:13 -0300

    feat: Add defer function to menu options in funcoes package

--- outline/funcoes/menu_funcoes.go
+++ outline/funcoes/menu_funcoes.go
@@ -6,5 +6,6 @@ func MenuFuncoes([]string) []format.MenuOptions {
 	return []format.MenuOptions{
 		{Options: "--sintaxe", ExecFunc: func() { Sintaxe() }},
 		{Options: "--desenrolando-enumerando-uma-slice", ExecFunc: func() { DesenrolandoEnumerandoUmaSlice() }},
+		{Options: "--defer", ExecFunc: func() { Defer() }},
 	}
 }
