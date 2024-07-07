2daab9073932d6b6aa965acfeca3f65cbf4f8db5
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-07-07 13:15:37 -0300

    feat: Add DesenrolandoEnumerandoUmaSlice function to menu options and funcoes package

--- outline/funcoes/menu_funcoes.go
+++ outline/funcoes/menu_funcoes.go
@@ -5,5 +5,6 @@ import "github.com/fabianoflorentino/aprendago/outline/format"
 func MenuFuncoes([]string) []format.MenuOptions {
 	return []format.MenuOptions{
 		{Options: "--sintaxe", ExecFunc: func() { Sintaxe() }},
+		{Options: "--desenrolando-enumerando-uma-slice", ExecFunc: func() { DesenrolandoEnumerandoUmaSlice() }},
 	}
 }
