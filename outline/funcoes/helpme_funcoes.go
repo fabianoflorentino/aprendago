d33f05bfdc5fcf4cfa41c4dc9c7436ed23e08df6
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-07-07 13:15:27 -0300

    feat: Add description for DesenrolandoEnumerandoUmaSlice function in HelpMeFuncoes
    refact: improve description for sintaxe option

--- outline/funcoes/helpme_funcoes.go
+++ outline/funcoes/helpme_funcoes.go
@@ -8,7 +8,8 @@ import (
 
 func HelpMeFuncoes() {
 	hlp := []format.HelpMe{
-		{Flag: "--sintaxe", Description: "Sintaxe", Width: 0},
+		{Flag: "--sintaxe", Description: "Sintaxe de uma função", Width: 0},
+		{Flag: "--desenroland-enumerando-uma-slice", Description: "Descreve como desenrolar (enumerar) uma slice", Width: 0},
 	}
 
 	fmt.Println("\nCapítulo 12: Funções")
