7c805c7b2a633fe498eb1f552727702eeeea397e
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-07-07 22:10:31 -0300

    feat: Update HelpMeFuncoes with improved descriptions

--- outline/funcoes/helpme_funcoes.go
+++ outline/funcoes/helpme_funcoes.go
@@ -7,10 +7,12 @@ import (
 )
 
 func HelpMeFuncoes() {
+	// hlp é uma slice de struct format.HelpMe que contém informações sobre comandos de ajuda.
 	hlp := []format.HelpMe{
-		{Flag: "--sintaxe", Description: "Sintaxe de uma função", Width: 0},
-		{Flag: "--desenroland-enumerando-uma-slice", Description: "Descreve como desenrolar (enumerar) uma slice", Width: 0},
+		{Flag: "--sintaxe", Description: "Sintaxe de declaração de função", Width: 0},
+		{Flag: "--desenroland-enumerando-uma-slice", Description: "Descreve como iterar (enumerar) uma slice", Width: 0},
 		{Flag: "--defer", Description: "Descreve o uso da declaração defer", Width: 0},
+		{Flag: "--metodos", Description: "Descreve o que são métodos em Go", Width: 0},
 	}
 
 	fmt.Println("\nCapítulo 12: Funções")
