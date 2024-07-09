d63b8a1a54f9cfde058a441c17aee468cd9217a3
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-07-09 16:06:18 -0300

    feat: Add interfaces and polymorphism description to HelpMeFuncoes

--- outline/funcoes/helpme_funcoes.go
+++ outline/funcoes/helpme_funcoes.go
@@ -13,6 +13,7 @@ func HelpMeFuncoes() {
 		{Flag: "--desenroland-enumerando-uma-slice", Description: "Descreve como iterar (enumerar) uma slice", Width: 0},
 		{Flag: "--defer", Description: "Descreve o uso da declaração defer", Width: 0},
 		{Flag: "--metodos", Description: "Descreve o que são métodos em Go", Width: 0},
+		{Flag: "--interfaces-e-polimorfismo", Description: "Descreve o que são interfaces e polimorfismo em Go", Width: 0},
 	}
 
 	fmt.Println("\nCapítulo 12: Funções")
