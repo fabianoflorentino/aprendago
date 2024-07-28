36a989d67d094a804712e00b34873ed79c7419ff
Daniel Moreto <dfmoreto@gmail.com>
2024-07-28 15:42:59 -0300

    feat: add new way to render outline on format package This new way keeps backward compatibility with previous way of rendering

--- pkg/format/outline_topic.go
+++ pkg/format/outline_topic.go
@@ -1,12 +1,96 @@
 package format
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 type OutlineContent struct {
 	Title   string
 	Content string
 }
 
+func (outlineContent *OutlineContent) AddHeader(content string) {
+	outlineContent.Content += strings.ToUpper(content) + "\n"
+}
+
+func (outlineContent *OutlineContent) AddText(content string) {
+	outlineContent.Content += content + "\n"
+}
+
+func (outlineContent *OutlineContent) StartList(callback func(*List)) {
+	list := List{level: 0}
+	callback(&list)
+	outlineContent.Content += list.render()
+}
+
+type listRendering interface {
+	render() string
+}
+
+type List struct {
+	level int
+	items []listRendering
+}
+
+func (currentList List) render() string {
+	var content string
+
+	for _, listItem := range currentList.items {
+		content += listItem.render()
+	}
+
+	return content
+}
+
+func (currentList *List) StartSubList(callback func(*List)) {
+	childList := List{level: currentList.level + 1}
+	currentList.items = append(currentList.items, &childList)
+	callback(&childList)
+}
+
+func (list *List) AddItem(content string) {
+	itemContent := itemContent{content, list}
+	list.items = append(list.items, &itemContent)
+}
+
+type itemContent struct {
+	content string
+	list    *List
+}
+
+func (itemContent itemContent) render() string {
+	tabs := strings.Repeat("\t", itemContent.list.level)
+	return tabs + "- " + itemContent.content + "\n"
+}
+
 func FormatOutlineTopic(topic OutlineContent) {
 	fmt.Printf("%v \n %v\n", topic.Title, topic.Content)
 }
+
+func StartSection(callback func(*OutlineContent)) {
+	section := OutlineContent{}
+	callback(&section)
+	fmt.Println(section.Content)
+}
+
+// func renderList(list *List) string {
+// 	var content string
+
+// 	for {
+// 		tabs := strings.Repeat("\t", list.level)
+// 		for _, listItem := range list.items {
+// 			content += tabs + "- " + listItem + "\n"
+// 		}
+
+// 		if len(list.childLists) <= 0 {
+// 			break
+// 		}
+
+// 		for _, childList := range list.childLists {
+// 			content += renderList(childList)
+// 		}
+// 	}
+
+// 	return content
+// }
