ae4f3d6db9ff7bc264d3e850365d0db90312ed6d
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-07-31 18:11:27 -0300

    feat: Add helpme_exercicicios_ninja_nivel_6.go to the repository

---
+++ internal/exercicios_ninja_nivel_6/helpme_exercicicios_ninja_nivel_6.go
@@ -0,0 +1,16 @@
+package exercicios_ninja_nivel_6
+
+import (
+	"fmt"
+
+	"github.com/fabianoflorentino/aprendago/pkg/format"
+)
+
+func HelMeExerciciosNinjaNivel6() {
+	hlp := []format.HelpMe{
+		{Flag: "--na-pratica-exercicio-1 --nivel-6", Description: "Apresenta o primeiro exercício prático do Nível 6.", Width: 0},
+	}
+
+	fmt.Println("\nCapítulo 12: Exercícios Ninja Nível 6")
+	format.PrintHelpMe(hlp)
+}
