7745bf64a00fb1392917c474019d979a473047d4
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-07-31 18:12:16 -0300

    feat: Add topics.go file for Exercícios Ninja Nível 6

---
+++ internal/exercicios_ninja_nivel_6/topics.go
@@ -0,0 +1,19 @@
+package exercicios_ninja_nivel_6
+
+import "fmt"
+
+func ExerciciosNinjaNivel6() {
+	fmt.Printf("Capítulo 12: Exercícios Ninja Nível 6\n\n")
+
+	NaPraticaExercicio1()
+	// NaPraticaExercicio2()
+	// NaPraticaExercicio3()
+	// NaPraticaExercicio4()
+	// NaPraticaExercicio5()
+	// NaPraticaExercicio6()
+	// NaPraticaExercicio7()
+	// NaPraticaExercicio8()
+	// NaPraticaExercicio9()
+	// NaPraticaExercicio10()
+	// NaPraticaExercicio11()
+}
