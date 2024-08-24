b700fdc1c770dfed3f6766785045c62bf2ff14e8
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-08-24 18:38:07 -0300

    feat: Add NaPraticaExercicio2, NaPraticaExercicio3, and their solutions for Exercícios Ninja Nível 6

--- internal/exercicios_ninja_nivel_6/helpme_exercicicios_ninja_nivel_6.go
+++ internal/exercicios_ninja_nivel_6/helpme_exercicicios_ninja_nivel_6.go
@@ -10,6 +10,10 @@ func HelMeExerciciosNinjaNivel6() {
 	hlp := []format.HelpMe{
 		{Flag: "--na-pratica-exercicio-1 --nivel-6", Description: "Apresenta o primeiro exercício prático do Nível 6.", Width: 0},
 		{Flag: "--na-pratica-exercicio-1 --nivel-6 --resolucao", Description: "Apresenta a resolução do primeiro exercício prático do Nível 6.", Width: 0},
+		{Flag: "--na-pratica-exercicio-2 --nivel-6", Description: "Apresenta o segundo exercício prático do Nível 6.", Width: 0},
+		{Flag: "--na-pratica-exercicio-2 --nivel-6 --resolucao", Description: "Apresenta a resolução do segundo exercício prático do Nível 6.", Width: 0},
+		{Flag: "--na-pratica-exercicio-3 --nivel-6", Description: "Apresenta o terceiro exercício prático do Nível 6.", Width: 0},
+		{Flag: "--na-pratica-exercicio-3 --nivel-6 --resolucao", Description: "Apresenta a resolução do terceiro exercício prático do Nível 6.", Width: 0},
 	}
 
 	fmt.Println("\nCapítulo 12: Exercícios Ninja Nível 6")
