3eff54e1fa0c39375f600a15e1f41c00adaaa222
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-08-24 18:37:57 -0300

    feat: Enable NaPraticaExercicio2 and NaPraticaExercicio3 in topics.go

--- internal/exercicios_ninja_nivel_6/topics.go
+++ internal/exercicios_ninja_nivel_6/topics.go
@@ -6,8 +6,8 @@ func ExerciciosNinjaNivel6() {
 	fmt.Printf("Capítulo 12: Exercícios Ninja Nível 6\n\n")
 
 	NaPraticaExercicio1()
-	// NaPraticaExercicio2()
-	// NaPraticaExercicio3()
+	NaPraticaExercicio2()
+	NaPraticaExercicio3()
 	// NaPraticaExercicio4()
 	// NaPraticaExercicio5()
 	// NaPraticaExercicio6()
