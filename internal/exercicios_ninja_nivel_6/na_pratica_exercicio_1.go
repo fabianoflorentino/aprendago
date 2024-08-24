27e80b6d0cead844aa8a610b643551d747d017fa
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-08-24 16:42:56 -0300

    feat: Update NaPraticaExercicio1 function and add solution link

--- internal/exercicios_ninja_nivel_6/na_pratica_exercicio_1.go
+++ internal/exercicios_ninja_nivel_6/na_pratica_exercicio_1.go
@@ -1,28 +1,66 @@
 package exercicios_ninja_nivel_6
 
-import "github.com/fabianoflorentino/aprendago/pkg/format"
+import (
+	"fmt"
+
+	"github.com/fabianoflorentino/aprendago/pkg/format"
+)
 
 func NaPraticaExercicio1() {
-	format.StartSection(func(outline *format.OutlineContent) {
-		outline.AddHeader("Na prática: Exercício #1")
-		outline.StartList(func(list *format.List) {
-			list.AddItem("Exercício:")
-			list.StartSubList(func(list *format.List) {
-				list.AddItem(("Crie uma função que retorne um int"))
-				list.AddItem(("Crie outra função que retorne um int e uma string"))
-				list.AddItem(("Chame as duas funções"))
-				list.AddItem(("Demonstre seus resultados"))
-			})
-		})
-		outline.StartList(func(list *format.List) {
-			list.AddItem("Solução: https://play.golang.org/p/rxJM5fgI-9")
-		})
+	topic := format.OutlineContent{
+		Title: "Na Prática - Exercício 1",
+		Content: `
+- Exercício:
+  - Crie uma função que retorne um int
+  - Crie outra função que retorne um int e uma string
+  - Chame as duas funções
+  - Demonstre seus resultados
+- Solução: https://play.golang.org/p/rxJM5fgI-9
 
-		outline.StartList(func(list *format.List) {
-			list.AddItem("Revisão:")
-		})
-	})
+- Revisão:
+  - Funções!
+    - Servem para abstrair código
+    - E para reutilizar código
+  - A ordem das coisas é:
+    - func (receiver) identifier (parameters) (returns) { code }
+  - Parâmetros vs. argumentos
+  - Funções variádicas
+    - Múltiplos parâmetros
+    - Múltiplos argumentos
+  - Métodos
+  - Interfaces & polimorfismo
+  - Defer
+    - "Deixa pra depois!"
+  - Returns
+    - Múltiplos returns
+    - Returns com nome (blé!)
+  - Funcs como expressões
+    - Atribuindo uma função a uma variável
+  - Callbacks
+    - Passando uma função como argumento para outra função
+  - Closure
+    - Capturando um scope
+    - Variáveis declaradas em scopes externos são visíveis em scopes internos
+  - Recursividade
+    - Uma função que chama a ela mesma
+    - Fatoriais
+    `,
+	}
+
+	format.FormatOutlineTopic(topic)
 }
 
 func ResolucaoNaPraticaExercicio1() {
+	numero_int := RetornaInt()
+	numero, texto := RetornaIntString()
+
+	fmt.Printf("Inteiro: %v\nInteiro&Texto: %v e %v", numero_int, numero, texto)
+}
+
+func RetornaInt() int {
+	return 42
+}
+
+func RetornaIntString() (int, string) {
+	return 42, "Olá, mundo!"
 }
