156050d63c59305d11d07d5364572fcfab6a1b5f
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-08-24 17:46:15 -0300

    feat: Add resolucao flag to HelpMeExerciciosNinjaNivel6 function

--- internal/exercicios_ninja_nivel_6/helpme_exercicicios_ninja_nivel_6.go
+++ internal/exercicios_ninja_nivel_6/helpme_exercicicios_ninja_nivel_6.go
@@ -9,6 +9,7 @@ import (
 func HelMeExerciciosNinjaNivel6() {
 	hlp := []format.HelpMe{
 		{Flag: "--na-pratica-exercicio-1 --nivel-6", Description: "Apresenta o primeiro exercício prático do Nível 6.", Width: 0},
+		{Flag: "--na-pratica-exercicio-1 --nivel-6 --resolucao", Description: "Apresenta a resolução do primeiro exercício prático do Nível 6.", Width: 0},
 	}
 
 	fmt.Println("\nCapítulo 12: Exercícios Ninja Nível 6")
