7c819ce2962a84107645e2319f18850e604dcb78
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-08-26 09:02:52 -0300

    feat: Add NaPraticaExercicio5 function for Exercícios Ninja Nível 6

---
+++ internal/exercicios_ninja_nivel_6/na_pratica_exercicio_5.go
@@ -0,0 +1,13 @@
+package exercicios_ninja_nivel_6
+
+import "github.com/fabianoflorentino/aprendago/pkg/format"
+
+func NaPraticaExercicio5() {
+	topic := format.OutlineContent{
+		Title: "Na Prática: Exercício #5",
+		Content: `
+		`,
+	}
+
+	format.FormatOutlineTopic(topic)
+}
