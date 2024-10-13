c900159c6b808577bdc4f79244007d07ba77b3ac
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-10-13 15:07:29 -0300

    feat: Add cap-2 menu to show all topic

--- internal/menu/capitulo_outline.go
+++ internal/menu/capitulo_outline.go
@@ -3,6 +3,7 @@ package menu
 import (
 	"fmt"
 
+	"github.com/fabianoflorentino/aprendago/internal/variaveis_valores_tipos"
 	"github.com/fabianoflorentino/aprendago/internal/visao_geral_do_curso"
 	"github.com/fabianoflorentino/aprendago/pkg/format"
 )
@@ -10,12 +11,14 @@ import (
 func MenuCapituloOutline([]string) []format.MenuOptions {
 	return []format.MenuOptions{
 		{Options: "--cap-1 --outline", ExecFunc: func() { visao_geral_do_curso.VisaoGeralDoCurso() }},
+		{Options: "--cap-2 --variaveis-valores-tipos", ExecFunc: func() { variaveis_valores_tipos.VariaveisValoresETipos() }},
 	}
 }
 
 func HelpMeCapituloOutline() {
 	hlp := []format.HelpMe{
 		{Flag: "--cap-1 --outline", Description: "Visão Geral do Curso", Width: 0},
+		{Flag: "--cap-2 --variaveis-valores-tipos", Description: "Variáveis, Valores & Tipos", Width: 0},
 	}
 
 	fmt.Printf("\nOutline do Curso por Capítulo\n")
