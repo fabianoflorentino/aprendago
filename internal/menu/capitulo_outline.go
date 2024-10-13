12c49ec7a2204b26a4b9a56d53f038b5b5b86f9b
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-10-13 15:38:20 -0300

    feat: Adicionar menu cap-3 para mostrar todos os tópicos
    Adiciona uma opção de menu para o capítulo 3, que executa a função ExerciciosNinjaNivel3 do pacote exercicios_ninja_nivel_3.

--- internal/menu/capitulo_outline.go
+++ internal/menu/capitulo_outline.go
@@ -3,6 +3,7 @@ package menu
 import (
 	"fmt"
 
+	"github.com/fabianoflorentino/aprendago/internal/exercicios_ninja_nivel_3"
 	"github.com/fabianoflorentino/aprendago/internal/variaveis_valores_tipos"
 	"github.com/fabianoflorentino/aprendago/internal/visao_geral_do_curso"
 	"github.com/fabianoflorentino/aprendago/pkg/format"
@@ -12,6 +13,7 @@ func MenuCapituloOutline([]string) []format.MenuOptions {
 	return []format.MenuOptions{
 		{Options: "--cap-1 --outline", ExecFunc: func() { visao_geral_do_curso.VisaoGeralDoCurso() }},
 		{Options: "--cap-2 --variaveis-valores-tipos", ExecFunc: func() { variaveis_valores_tipos.VariaveisValoresETipos() }},
+		{Options: "--cap-3 --exercicios-ninja-nivlel-1", ExecFunc: func() { exercicios_ninja_nivel_3.ExerciciosNinjaNivel3() }},
 	}
 }
 
@@ -19,6 +21,7 @@ func HelpMeCapituloOutline() {
 	hlp := []format.HelpMe{
 		{Flag: "--cap-1 --outline", Description: "Visão Geral do Curso", Width: 0},
 		{Flag: "--cap-2 --variaveis-valores-tipos", Description: "Variáveis, Valores & Tipos", Width: 0},
+		{Flag: "--cap-3 --exercicios-ninja-nivlel-1", Description: "Exercícios Ninja Nível 1", Width: 0},
 	}
 
 	fmt.Printf("\nOutline do Curso por Capítulo\n")
