0c277342d2067a0fbc203dfa2efc190d2ef280bb
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-10-14 23:15:52 -0300

    feat: Adicionar menu cap-4 para mostrar todos os tópicos de Fundamentos da Programação

--- internal/menu/capitulo_outline.go
+++ internal/menu/capitulo_outline.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/fabianoflorentino/aprendago/internal/exercicios_ninja_nivel_3"
+	"github.com/fabianoflorentino/aprendago/internal/fundamentos_da_programacao"
 	"github.com/fabianoflorentino/aprendago/internal/variaveis_valores_tipos"
 	"github.com/fabianoflorentino/aprendago/internal/visao_geral_do_curso"
 	"github.com/fabianoflorentino/aprendago/pkg/format"
@@ -14,6 +15,7 @@ func MenuCapituloOutline([]string) []format.MenuOptions {
 		{Options: "--cap-1 --outline", ExecFunc: func() { visao_geral_do_curso.VisaoGeralDoCurso() }},
 		{Options: "--cap-2 --variaveis-valores-tipos", ExecFunc: func() { variaveis_valores_tipos.VariaveisValoresETipos() }},
 		{Options: "--cap-3 --exercicios-ninja-nivlel-1", ExecFunc: func() { exercicios_ninja_nivel_3.ExerciciosNinjaNivel3() }},
+		{Options: "--cap-4 --fundamentos-da-programacao", ExecFunc: func() { fundamentos_da_programacao.FundamentosDaProgramacao() }},
 	}
 }
 
@@ -22,6 +24,7 @@ func HelpMeCapituloOutline() {
 		{Flag: "--cap-1 --outline", Description: "Visão Geral do Curso", Width: 0},
 		{Flag: "--cap-2 --variaveis-valores-tipos", Description: "Variáveis, Valores & Tipos", Width: 0},
 		{Flag: "--cap-3 --exercicios-ninja-nivlel-1", Description: "Exercícios Ninja Nível 1", Width: 0},
+		{Flag: "--cap-4 --fundamentos-da-programacao", Description: "Fundamentos da Programação", Width: 0},
 	}
 
 	fmt.Printf("\nOutline do Curso por Capítulo\n")
