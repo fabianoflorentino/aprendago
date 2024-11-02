8cf1fe2da43772b2d520d726dfce8389683d4d67
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-11-02 17:26:37 -0300

    feat: Adicionar função para construir seções a partir de diretórios e titulo

---
+++ pkg/format/section.go
@@ -0,0 +1,32 @@
+package format
+
+import (
+	"log"
+	"os"
+
+	"github.com/fabianoflorentino/aprendago/pkg/reader"
+)
+
+func BuildSection(dir string, title string) {
+	section, err := reader.ReadSection(dir, title)
+	if err != nil {
+		if os.IsNotExist(err) {
+			log.Fatalf("Seção '%s' não encontrada no diretório '%s'", title, dir)
+		} else {
+			log.Fatalf("Erro ao ler a seção: %v", err)
+		}
+	}
+
+	document := reader.Document{
+		Description: reader.Description{
+			Chapter: reader.Chapter{
+				Sections: []reader.Section{*section},
+			},
+		},
+	}
+
+	err = FormatDocument([]reader.Document{document})
+	if err != nil {
+		log.Fatalf("Erro ao formatar o documento: %v", err)
+	}
+}
