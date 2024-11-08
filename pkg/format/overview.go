6af793f38d87b54d629c4b09a17e48a2d64de727
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-11-07 22:55:01 -0300

    refactor: Adicionar documentação para o pacote format e suas funções de formatação de documentos

--- pkg/format/overview.go
+++ pkg/format/overview.go
@@ -1,3 +1,6 @@
+/*
+Package format provides functions to format documents. The documents are printed to os.Stdout.
+*/
 package format
 
 import (
@@ -7,6 +10,9 @@ import (
 	"github.com/fabianoflorentino/aprendago/pkg/reader"
 )
 
+// documentTemplate is a template for the overview of a document.
+// It prints the name of the document and all its sections.
+// The sections are printed with their title and text.
 const documentTemplate = `
 {{- .Document.Description.Name}}
 {{range .Document.Description.Sections}}
@@ -16,6 +22,9 @@ const documentTemplate = `
 {{end -}}
 `
 
+// FormatOverview prints an overview of the given documents to os.Stdout.
+// The overview includes the name of the document and all its sections.
+// The sections are printed with their title and text.
 func FormatOverview(documents []reader.Document) error {
 	tmpl, err := template.New("document").Parse(documentTemplate)
 	if err != nil {
