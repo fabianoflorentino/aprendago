a6bcf8a09645c80dbc342c5fe238692e10032de0
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-11-07 22:56:56 -0300

    refactor: Adicionar documentação para o pacote format e a função FormatSection

--- pkg/format/section.go
+++ pkg/format/section.go
@@ -1,3 +1,6 @@
+/*
+Package format provides methods to format a document or a section of a document
+*/
 package format
 
 import (
@@ -7,6 +10,15 @@ import (
 	"github.com/fabianoflorentino/aprendago/pkg/reader"
 )
 
+// FormatSection formats a section of a document
+//   - dir: directory where the document is located
+//   - title: title of the section to be formatted
+//
+// The method reads the section from the file and formats the document overview
+// If the section is not found, the method logs an error message
+// If an error occurs while reading the section, the method logs an error message
+// If an error occurs while formatting the document, the method logs an error message
+// and returns the error
 func FormatSection(dir string, title string) {
 	section, err := reader.ReadSection(dir, title)
 	if err != nil {
