5aaf5f55c749efcb11d18a09308b1e107207bb5f
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-11-09 18:31:59 -0300

    refactor: remove outdated structs and related functions, and introduce overview.yml for better organization

--- internal/structs/topics.go
+++ internal/structs/topics.go
@@ -1,12 +1,43 @@
 package structs
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/fabianoflorentino/aprendago/pkg/format"
+)
+
+const rootDir = "internal/structs"
 
 func TopicStructs() {
 	fmt.Print("10 - Structs\n\n")
 
-	Struct()
-	StructsEmbutidos()
-	LendoADocumentacao()
-	StructsAnonimos()
+	executeSection("Structs")
+	executeSection("Structs embutidos")
+	executeSection("Lendo a documentação")
+	executeSection("Structs anônimos")
+}
+
+func MenuStructs([]string) []format.MenuOptions {
+	return []format.MenuOptions{
+		{Options: "--structs", ExecFunc: func() { executeSection("Structs") }},
+		{Options: "--structs-embutidos", ExecFunc: func() { executeSection("Structs embutidos") }},
+		{Options: "--lendo-a-documentacao", ExecFunc: func() { executeSection("Lendo a documentação") }},
+		{Options: "--structs-anonimos", ExecFunc: func() { executeSection("Structs anônimos") }},
+	}
+}
+
+func HelpMeStructs() {
+	hlp := []format.HelpMe{
+		{Flag: "--structs", Description: "Structs", Width: 0},
+		{Flag: "--structs-embutidos", Description: "Structs Embutidos", Width: 0},
+		{Flag: "--lendo-a-documentacao", Description: "Lendo a documentação", Width: 0},
+		{Flag: "--structs-anonimos", Description: "Structs Anônimos", Width: 0},
+	}
+
+	fmt.Println("\n Capítulo 10: Structs")
+	format.PrintHelpMe(hlp)
+}
+
+func executeSection(section string) {
+	format.FormatSection(rootDir, section)
 }
