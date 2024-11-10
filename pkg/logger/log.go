2a513fe6220acfc3c47db5fff89930bc3d097738
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-11-09 22:22:03 -0300

    docs: add documentation for Log function in logger package

--- pkg/logger/log.go
+++ pkg/logger/log.go
@@ -1,3 +1,7 @@
+/*
+Package logger provides a method to log messages to a file and print them to the console
+  - Log: writes a log message to a file and prints it to the console
+*/
 package logger
 
 import (
@@ -6,6 +10,13 @@ import (
 	"os"
 )
 
+// Log writes a log message to a file and prints it to the console
+//   - format: format of the log message
+//   - args: arguments to be formatted
+//
+// The method opens a file in the log directory and writes the log message
+// If an error occurs while opening the file, the method logs an error message
+// and exits the program
 func Log(format string, args ...any) {
 	file, err := os.OpenFile("log/"+os.Getenv("GOENV")+".log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
 	if err != nil {
