9bdf355b588ff0ffb8498de94d4412b88d8d7721
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-11-20 13:35:25 -0300

    feat: add examples to marshal, unmarshal, and encode methods

--- internal/aplicacoes/topics.go
+++ internal/aplicacoes/topics.go
@@ -24,7 +24,9 @@ func MenuAplicacoes([]string) []format.MenuOptions {
 
 	return []format.MenuOptions{
 		{Options: "--documentacao-json", ExecFunc: func() { executeSections("Documentação JSON") }},
-		{Options: "--documentacao-json --example", ExecFunc: func() { PrintExampleJSON() }},
+		{Options: "--documentacao-json --example --json-marshal", ExecFunc: func() { UsingJsonMarshal() }},
+		{Options: "--documentacao-json --example --json-unmarshal", ExecFunc: func() { UsingJsonUnmarshal() }},
+		{Options: "--documentacao-json --example --json-encoder", ExecFunc: func() { UsingJsonEncoder() }},
 		{Options: "--json-marshal", ExecFunc: func() { executeSections("JSON marshal (ordenação)") }},
 		{Options: "--json-unmarshal", ExecFunc: func() { executeSections("JSON unmarshal (desornação)") }},
 		{Options: "--interface-writer", ExecFunc: func() { executeSections("A interface Writer") }},
@@ -37,8 +39,9 @@ func MenuAplicacoes([]string) []format.MenuOptions {
 func HelpMeAplicacoes() {
 	hlp := []format.HelpMe{
 		{Flag: "--documentacao-json", Description: "Descreve como documentar um pacote em Go", Width: 0},
-		{Flag: "--json-marshal", Description: "Descreve como ordenar um JSON", Width: 0},
-		{Flag: "--json-unmarshal", Description: "Descreve como desordenar um JSON", Width: 0},
+		{Flag: "--documentacao-json --example --json-marshal", Description: "Exemplo de como ordenar um JSON", Width: 0},
+		{Flag: "--documentacao-json --example --json-unmarshal", Description: "Exemplo de como desordenar um JSON", Width: 0},
+		{Flag: "--documentacao-json --example --json-encoder", Description: "Exemplo de como usar o encoder JSON", Width: 0},
 		{Flag: "--interface-writer", Description: "Descreve o que é a interface Writer", Width: 0},
 		{Flag: "--pacote-sort", Description: "Descreve o pacote sort", Width: 0},
 		{Flag: "--customizando-sort", Description: "Descreve como customizar o pacote sort", Width: 0},
