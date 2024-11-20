32d03e8a981e0d2a03b1f84cec88fdf6a2768961
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-11-20 13:34:18 -0300

    feat: implement JSON marshalling and unmarshalling examples for pessoa struct

--- internal/aplicacoes/examples.go
+++ internal/aplicacoes/examples.go
@@ -2,30 +2,55 @@ package aplicacoes
 
 import (
 	"encoding/json"
+	"fmt"
 	"os"
 )
 
-type exampleJSON struct {
+type pessoa struct {
 	Name     string
 	Lastname string
 	Age      int
 }
 
-func PrintExampleJSON() {
-	toJSON, err := json.Marshal(showExampleJSON())
+type extractInfo struct {
+	Name     string `json:"Name"`
+	Lastname string `json:"Lastname"`
+	Age      int    `json:"Age"`
+}
+
+func UsingJsonMarshal() {
+	p := pessoa{
+		Name:     "Fabiano",
+		Lastname: "Florentino",
+		Age:      40,
+	}
+
+	pessoaToJSON, err := json.Marshal(p)
 	if err != nil {
 		panic(err)
 	}
 
-	os.Stdout.Write(toJSON)
+	os.Stdout.Write(pessoaToJSON)
 }
 
-func showExampleJSON() exampleJSON {
-	person := exampleJSON{
-		Name:     "John",
-		Lastname: "Doe",
-		Age:      30,
+func UsingJsonUnmarshal() {
+	pessoaJSON := []byte(`{"Name":"Fabiano","Lastname":"Florentino","Age":40}`)
+
+	var p extractInfo
+	err := json.Unmarshal(pessoaJSON, &p)
+	if err != nil {
+		panic(err)
 	}
 
-	return person
+	fmt.Printf("Name: %s\nLastname: %s\nAge: %d\n", p.Name, p.Lastname, p.Age)
+}
+
+func UsingJsonEncoder() {
+	p := pessoa{
+		Name:     "Fabiano",
+		Lastname: "Florentino",
+		Age:      40,
+	}
+
+	json.NewEncoder(os.Stdout).Encode(p)
 }
