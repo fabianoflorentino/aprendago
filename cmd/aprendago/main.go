4ff43c567635b5d11c2838ebe28985c1c2189bbf
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-11-26 09:45:47 -0300

    feat: add detailed comments to main function explaining argument handling and menu options

--- cmd/aprendago/main.go
+++ cmd/aprendago/main.go
@@ -1,4 +1,12 @@
-// Aprenda Go: https://youtube.com/playlist?list=PLCKpcjBB_VlBsxJ9IseNxFllf-UFEXOdg&si=_JIbmByhwYvHdJAr
+/*
+Main function to start the application and call the menu options based on the arguments
+passed by the user.
+If no arguments are passed, the HelpMe function is called to show the available options.
+If arguments are passed, the Options function is called to execute the requested option.
+  - os.Args[1:] is used to pass all arguments except the first one, which is the name of the program.
+    This way, the Options function receives only the arguments that are relevant to the requested option.
+    For example, if the user types "--outline", the Options function will receive only "--outline".
+*/
 package main
 
 import (
