2debf9407f97b1bcf93a8b8c08edd54fa5a827df
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-11-27 14:27:44 -0300

    feat: add chapters 8 to 10 options and descriptions to the course outline menu

--- internal/menu/capitulo_outline.go
+++ internal/menu/capitulo_outline.go
@@ -3,10 +3,13 @@ package menu
 import (
 	"fmt"
 
+	"github.com/fabianoflorentino/aprendago/internal/agrupamento_de_dados"
 	"github.com/fabianoflorentino/aprendago/internal/exercicios_ninja_nivel_2"
 	"github.com/fabianoflorentino/aprendago/internal/exercicios_ninja_nivel_3"
+	"github.com/fabianoflorentino/aprendago/internal/exercicios_ninja_nivel_4"
 	"github.com/fabianoflorentino/aprendago/internal/fluxo_de_controle"
 	"github.com/fabianoflorentino/aprendago/internal/fundamentos_da_programacao"
+	"github.com/fabianoflorentino/aprendago/internal/structs"
 	"github.com/fabianoflorentino/aprendago/internal/variaveis_valores_tipos"
 	"github.com/fabianoflorentino/aprendago/internal/visao_geral_do_curso"
 	"github.com/fabianoflorentino/aprendago/pkg/format"
@@ -21,6 +24,9 @@ func MenuCapituloOutline([]string) []format.MenuOptions {
 		{Options: "--cap-5 --overview", ExecFunc: func() { exercicios_ninja_nivel_2.ExerciciosNinjaNivel2() }},
 		{Options: "--cap-6 --overview", ExecFunc: func() { fluxo_de_controle.FluxoDeControle() }},
 		{Options: "--cap-7 --overview", ExecFunc: func() { exercicios_ninja_nivel_3.ExerciciosNinjaNivel3() }},
+		{Options: "--cap-8 --overview", ExecFunc: func() { agrupamento_de_dados.AgrupamentoDeDados() }},
+		{Options: "--cap-9 --overview", ExecFunc: func() { exercicios_ninja_nivel_4.ExerciciosNinjaNivel4() }},
+		{Options: "--cap-10 --overview", ExecFunc: func() { structs.TopicStructs() }},
 	}
 }
 
@@ -33,6 +39,9 @@ func HelpMeCapituloOutline() {
 		{Flag: "--cap-5 --overview", Description: "Exercícios Ninja Nível 2", Width: 0},
 		{Flag: "--cap-6 --overview", Description: "Fluxo de Controle", Width: 0},
 		{Flag: "--cap-7 --overview", Description: "Exercícios Ninja Nível 3", Width: 0},
+		{Flag: "--cap-8 --overview", Description: "Agrupamento de Dados", Width: 0},
+		{Flag: "--cap-9 --overview", Description: "Exercícios Ninja Nível 4", Width: 0},
+		{Flag: "--cap-10 --overview", Description: "Structs", Width: 0},
 	}
 
 	fmt.Printf("\nOutline do Curso por Capítulo\n")
