cab8650e7c7ef3c7f853365dad23197035a8da79
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-11-29 17:43:17 -0300

    feat: expand "Seu Ambiente de Desenvolvimento" with shell commands and terminology

--- internal/seu_ambiente_de_desenvolvimento/topics.go
+++ internal/seu_ambiente_de_desenvolvimento/topics.go
@@ -1,20 +1,31 @@
 package seu_ambiente_de_desenvolvimento
 
-import "github.com/fabianoflorentino/aprendago/pkg/format"
+import (
+	"fmt"
+
+	"github.com/fabianoflorentino/aprendago/pkg/format"
+)
 
 var rootDir = "internal/seu_ambiente_de_desenvolvimento"
 
 func SeuAmbienteDeDesenvolvimento() {
+	fmt.Print("\n\nCapítulo 19: Seu Ambiente de Desenvolvimento\n")
+
 	executeSection("O terminal")
 }
 
 func MenuSeuAmbienteDeDesenvolvimento([]string) []format.MenuOptions {
-	return []format.MenuOptions{}
+	return []format.MenuOptions{
+		{Options: "--o-terminal", ExecFunc: func() { executeSection("O terminal") }},
+	}
 }
 
 func HelpMeSeuAmbienteDeDesenvolvimento() {
-	hlp := []format.HelpMe{}
+	hlp := []format.HelpMe{
+		{Flag: "--o-terminal", Description: "Exibe informações sobre o terminal."},
+	}
 
+	fmt.Println("\nCapítulo 19: Seu Ambiente de Desenvolvimento")
 	format.PrintHelpMe(hlp)
 }
 
