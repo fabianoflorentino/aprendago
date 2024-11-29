b9b6849f8d3aebd5090726848e1c40eb7c232dd8
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-11-29 19:29:49 -0300

    feat: add sections for Go workspace, environment variables, and IDEs to "Seu Ambiente de Desenvolvimento"

--- internal/seu_ambiente_de_desenvolvimento/topics.go
+++ internal/seu_ambiente_de_desenvolvimento/topics.go
@@ -12,17 +12,23 @@ func SeuAmbienteDeDesenvolvimento() {
 	fmt.Print("\n\nCapítulo 19: Seu Ambiente de Desenvolvimento\n")
 
 	executeSection("O terminal")
+	executeSection("Go workspace & environment variables")
+	executeSection("IDEs")
 }
 
 func MenuSeuAmbienteDeDesenvolvimento([]string) []format.MenuOptions {
 	return []format.MenuOptions{
 		{Options: "--o-terminal", ExecFunc: func() { executeSection("O terminal") }},
+		{Options: "--go-workspace-environment-variables", ExecFunc: func() { executeSection("Go workspace & environment variables") }},
+		{Options: "--ides", ExecFunc: func() { executeSection("IDEs") }},
 	}
 }
 
 func HelpMeSeuAmbienteDeDesenvolvimento() {
 	hlp := []format.HelpMe{
 		{Flag: "--o-terminal", Description: "Exibe informações sobre o terminal."},
+		{Flag: "--go-workspace-environment-variables", Description: "Exibe informações sobre o workspace e variáveis de ambiente do Go."},
+		{Flag: "--ides", Description: "Exibe informações sobre IDEs."},
 	}
 
 	fmt.Println("\nCapítulo 19: Seu Ambiente de Desenvolvimento")
