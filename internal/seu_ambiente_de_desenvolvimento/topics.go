5e33461e824b50595b046fb5eaf631df37792d28
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-11-29 23:33:27 -0300

    feat: Add sections on Go commands, GitHub repositories, GitHub exploration, cross-compilation, and packages to "Your Development Environment"

--- internal/seu_ambiente_de_desenvolvimento/topics.go
+++ internal/seu_ambiente_de_desenvolvimento/topics.go
@@ -14,6 +14,11 @@ func SeuAmbienteDeDesenvolvimento() {
 	executeSection("O terminal")
 	executeSection("Go workspace & environment variables")
 	executeSection("IDEs")
+	executeSection("comandos go")
+	executeSection("Repositórios no github")
+	executeSection("Explorando o github")
+	executeSection("Compilação cruzada")
+	executeSection("Pacotes")
 }
 
 func MenuSeuAmbienteDeDesenvolvimento([]string) []format.MenuOptions {
@@ -21,6 +26,11 @@ func MenuSeuAmbienteDeDesenvolvimento([]string) []format.MenuOptions {
 		{Options: "--o-terminal", ExecFunc: func() { executeSection("O terminal") }},
 		{Options: "--go-workspace-environment-variables", ExecFunc: func() { executeSection("Go workspace & environment variables") }},
 		{Options: "--ides", ExecFunc: func() { executeSection("IDEs") }},
+		{Options: "--comandos-go", ExecFunc: func() { executeSection("comandos go") }},
+		{Options: "--repositorios-no-github", ExecFunc: func() { executeSection("Repositórios no github") }},
+		{Options: "--explorando-o-github", ExecFunc: func() { executeSection("Explorando o github") }},
+		{Options: "--compilacao-cruzada", ExecFunc: func() { executeSection("Compilação cruzada") }},
+		{Options: "--pacotes", ExecFunc: func() { executeSection("Pacotes") }},
 	}
 }
 
@@ -29,6 +39,11 @@ func HelpMeSeuAmbienteDeDesenvolvimento() {
 		{Flag: "--o-terminal", Description: "Exibe informações sobre o terminal."},
 		{Flag: "--go-workspace-environment-variables", Description: "Exibe informações sobre o workspace e variáveis de ambiente do Go."},
 		{Flag: "--ides", Description: "Exibe informações sobre IDEs."},
+		{Flag: "--comandos-go", Description: "Exibe informações sobre comandos go."},
+		{Flag: "--repositorios-no-github", Description: "Exibe informações sobre repositórios no github."},
+		{Flag: "--explorando-o-github", Description: "Exibe informações sobre explorando o github."},
+		{Flag: "--compilacao-cruzada", Description: "Exibe informações sobre compilação cruzada."},
+		{Flag: "--pacotes", Description: "Exibe informações sobre pacotes."},
 	}
 
 	fmt.Println("\nCapítulo 19: Seu Ambiente de Desenvolvimento")
