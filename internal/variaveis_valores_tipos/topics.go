64c809f297091fb5ea6c68ce1e886429ef76ca01
Daniel Moreto <dfmoreto@gmail.com>
2024-11-30 20:40:53 -0300

    feat: remove unused functions from packages variaveis_valores_tipos and visao_geral_do_curso

--- internal/variaveis_valores_tipos/topics.go
+++ internal/variaveis_valores_tipos/topics.go
@@ -36,24 +36,6 @@ func MenuVariaveisValoresTipos([]string) []format.MenuOptions {
 	}
 }
 
-func HelpMeVariaveisValoresTipos() {
-
-	hlp := []format.HelpMe{
-		{Flag: "--go-playground", Description: "Exibe o Go Playground.", Width: 0},
-		{Flag: "--hello-world", Description: "Exibe o Hello World.", Width: 0},
-		{Flag: "--operador-curto-de-declaracao", Description: "Exibe o operador curto de declaração.", Width: 0},
-		{Flag: "--a-palavra-reservada-var", Description: "Exibe a palavra reservada var.", Width: 0},
-		{Flag: "--explorando-tipos", Description: "Exibe como explorar tipos.", Width: 0},
-		{Flag: "--valor-zero", Description: "Exibe o valor zero.", Width: 0},
-		{Flag: "--o-pacote-fmt", Description: "Exibe o pacote fmt.", Width: 0},
-		{Flag: "--criando-seu-proprio-tipo", Description: "Exibe como criar seu próprio tipo.", Width: 0},
-		{Flag: "--conversao-nao-coercao", Description: "Exibe a conversão não coerção.", Width: 0},
-	}
-
-	fmt.Println("\nCapítulo 2: Variáveis, Valores e Tipos")
-	format.PrintHelpMe(hlp)
-}
-
 func executeSection(section string) {
 	format.FormatSection(rootDir, section)
 }
--- internal/visao_geral_do_curso/topics.go
+++ internal/visao_geral_do_curso/topics.go
@@ -28,19 +28,6 @@ func MenuVisaoGeralDoCurso([]string) []format.MenuOptions {
 	}
 }
 
-func HelpMeVisaoGeralDoCurso() {
-	hlp := []format.HelpMe{
-		{Flag: "--bem-vindo", Description: "Exibe a mensagem de boas-vindas ao curso Aprenda Go.", Width: 0},
-		{Flag: "--porque-go", Description: "Descreve os benefícios e razões para aprender a linguagem Go.", Width: 0},
-		{Flag: "--sucesso", Description: "Apresenta dicas e estratégias para ter sucesso no curso.", Width: 0},
-		{Flag: "--recursos", Description: "Lista recursos e materiais de apoio para o curso.", Width: 0},
-		{Flag: "--como-esse-curso-funciona", Description: "Explica a estrutura e metodologia do curso.", Width: 0},
-	}
-
-	fmt.Println("\nCapítulo 1: Visão Geral do Curso")
-	format.PrintHelpMe(hlp)
-}
-
 func executeSection(section string) {
 	format.FormatSection(rootDir, section)
 }
