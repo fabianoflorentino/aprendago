36e120af599d610d963ed69f6cc48d03480ccff0
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-12-02 16:33:17 -0300

    feat: add detailed documentation for functions in the exercicios_ninja_nivel_1 package

--- internal/exercicios_ninja_nivel_1/topics.go
+++ internal/exercicios_ninja_nivel_1/topics.go
@@ -1,3 +1,8 @@
+// Package exercicios_ninja_nivel_1 provides functions to execute and display
+// exercises for the Ninja Level 1 course. It includes functionalities to
+// present exercises, show their resolutions, and provide help information
+// about the exercises. The package interacts with the format package to
+// format and display sections and help information.
 package exercicios_ninja_nivel_1
 
 import (
@@ -6,8 +11,19 @@ import (
 	"github.com/fabianoflorentino/aprendago/pkg/format"
 )
 
-const rootDir = "internal/exercicios_ninja_nivel_1"
+// rootDir represents the relative path to the directory containing
+// the exercises for Ninja Level 1 in the "aprendago" project.
+// This constant is used to reference the directory where the
+// exercise files are stored within the internal package structure.
+const (
+	rootDir = "internal/exercicios_ninja_nivel_1"
+)
 
+// ExerciciosNinjaNivel1 prints a header for "Ninja Level 1 Exercises" and
+// executes a series of sections related to practical exercises. Each section
+// is executed by calling the executeSection function with a specific exercise
+// description as an argument. This function serves as an entry point to
+// demonstrate and run various exercises for Ninja Level 1.
 func ExerciciosNinjaNivel1() {
 	fmt.Printf("\n\n03 - Exercicios: Ninja Nível 1\n")
 
@@ -20,6 +36,22 @@ func ExerciciosNinjaNivel1() {
 	executeSection("Na prática - Exercício #6")
 }
 
+// MenuExerciciosNinjaNivel1 returns a slice of format.MenuOptions for the Ninja Level 1 exercises.
+// Each menu option includes a command-line option string and an associated execution function.
+// The available options include:
+// - "--contribua-seu-codigo": Executes the "Contribua seu código" section.
+// - "--na-pratica-exercicio-1 --nivel-1": Executes the "Na prática - Exercício #1" section.
+// - "--na-pratica-exercicio-1 --nivel-1 --resolucao": Executes the resolution for "Na prática - Exercício #1".
+// - "--na-pratica-exercicio-2 --nivel-1": Executes the "Na prática - Exercício #2" section.
+// - "--na-pratica-exercicio-2 --nivel-1 --resolucao": Executes the resolution for "Na prática - Exercício #2".
+// - "--na-pratica-exercicio-3 --nivel-1": Executes the "Na prática - Exercício #3" section.
+// - "--na-pratica-exercicio-3 --nivel-1 --resolucao": Executes the resolution for "Na prática - Exercício #3".
+// - "--na-pratica-exercicio-4 --nivel-1": Executes the "Na prática - Exercício #4" section.
+// - "--na-pratica-exercicio-4 --nivel-1 --resolucao": Executes the resolution for "Na prática - Exercício #4".
+// - "--na-pratica-exercicio-5 --nivel-1": Executes the "Na prática - Exercício #5" section.
+// - "--na-pratica-exercicio-5 --nivel-1 --resolucao": Executes the resolution for "Na prática - Exercício #5".
+// - "--na-pratica-exercicio-6 --nivel-1": Executes the "Na prática - Exercício #6" section.
+// - "--na-pratica-exercicio-6 --nivel-1 --prova": Executes the "Responda a Prova" section.
 func MenuExerciciosNinjaNivel1([]string) []format.MenuOptions {
 	return []format.MenuOptions{
 		{Options: "--contribua-seu-codigo", ExecFunc: func() { executeSection("Contribua seu código") }},
@@ -38,6 +70,12 @@ func MenuExerciciosNinjaNivel1([]string) []format.MenuOptions {
 	}
 }
 
+// HelpMeExerciciosNinjaNivel1 prints a list of available commands and their descriptions
+// for the "Exercícios Ninja Nível 1" section of the course. Each command is associated
+// with a flag and a description that explains what the command does. The function
+// organizes these commands into a slice of HelpMe structs and then prints them using
+// the format.PrintHelpMe function. This helps users understand what exercises and
+// resolutions are available for this level of the course.
 func HelpMeExerciciosNinjaNivel1() {
 	hlp := []format.HelpMe{
 		{Flag: "--na-pratica-exercicio-1 --nivel-1", Description: "Apresenta o primeiro exercício prático do curso.", Width: 0},
@@ -58,6 +96,10 @@ func HelpMeExerciciosNinjaNivel1() {
 	format.PrintHelpMe(hlp)
 }
 
+// executeSection formats and processes a given section of the project.
+// It takes a section name as a string and uses the FormatSection function
+// from the format package to apply formatting to the specified section
+// within the root directory.
 func executeSection(section string) {
 	format.FormatSection(rootDir, section)
 }
