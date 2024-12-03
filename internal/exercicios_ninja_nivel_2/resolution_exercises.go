dd9553f36b360c88314575e74171301949541616
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-12-03 08:49:16 -0300

    feat: add detailed documentation for functions in the exercicios_ninja_nivel_2 package

--- internal/exercicios_ninja_nivel_2/resolution_exercises.go
+++ internal/exercicios_ninja_nivel_2/resolution_exercises.go
@@ -1,3 +1,8 @@
+// Package exercicios_ninja_nivel_2 provides solutions to various exercises
+// and a questionnaire related to Go programming language concepts. The
+// exercises cover topics such as constants, bitwise operations, and string
+// formatting. The questionnaire includes multiple-choice questions to test
+// knowledge on Go language fundamentals.
 package exercicios_ninja_nivel_2
 
 import (
@@ -6,6 +11,9 @@ import (
 	"github.com/fabianoflorentino/aprendago/pkg/format"
 )
 
+// ResolucaoNaPraticaExercicio1 prints the number 1000019 in decimal, binary, and hexadecimal formats.
+// It formats the number using fmt.Sprintf and then passes the formatted string to the
+// FormatResolucaoExercicios function from the format package.
 func ResolucaoNaPraticaExercicio1() {
 	numero := 1000019
 
@@ -14,6 +22,17 @@ func ResolucaoNaPraticaExercicio1() {
 	format.FormatResolucaoExercicios(resoluca)
 }
 
+// ResolucaoNaPraticaExercicio2 demonstrates the use of comparison operators in Go.
+// It initializes two integer variables, `a` and `b`, with values 10 and 20 respectively.
+// The function then creates a formatted string `resolucao` that includes the values of `a` and `b`,
+// as well as the results of various comparison operations between `a` and `b`:
+// - a == b: checks if `a` is equal to `b`
+// - a != b: checks if `a` is not equal to `b`
+// - a <= b: checks if `a` is less than or equal to `b`
+// - a < b: checks if `a` is less than `b`
+// - a >= b: checks if `a` is greater than or equal to `b`
+// - a > b: checks if `a` is greater than `b`
+// Finally, it calls the `FormatResolucaoExercicios` function from the `format` package to format and display the results.
 func ResolucaoNaPraticaExercicio2() {
 	a := 10
 	b := 20
@@ -23,6 +42,10 @@ func ResolucaoNaPraticaExercicio2() {
 	format.FormatResolucaoExercicios(resolucao)
 }
 
+// ResolucaoNaPraticaExercicio3 demonstrates the use of both untyped and typed constants in Go.
+// It defines two constants: `untypedConst` with an untyped value of 10, and `typedConst` with a typed value of 20.
+// The function then formats these constants into a string using `fmt.Sprintf` and passes the formatted string
+// to the `FormatResolucaoExercicios` function from the `format` package for further processing or display.
 func ResolucaoNaPraticaExercicio3() {
 	const (
 		untypedConst     = 10
@@ -34,6 +57,13 @@ func ResolucaoNaPraticaExercicio3() {
 	format.FormatResolucaoExercicios(resolucao)
 }
 
+// ResolucaoNaPraticaExercicio4 demonstrates the conversion of an integer value
+// to its decimal, binary, and hexadecimal representations. It performs the following steps:
+// 1. Initializes an integer variable v1 with the value 10.
+// 2. Formats the value of v1 into a string showing its decimal, binary, and hexadecimal forms.
+// 3. Left shifts the value of v1 by 1 bit to obtain v2.
+// 4. Formats the value of v2 into a string showing its decimal, binary, and hexadecimal forms.
+// 5. Concatenates the formatted strings of v1 and v2 and passes the result to the FormatResolucaoExercicios function for further processing or display.
 func ResolucaoNaPraticaExercicio4() {
 	v1 := 10
 
@@ -46,12 +76,20 @@ func ResolucaoNaPraticaExercicio4() {
 	format.FormatResolucaoExercicios(resolucao1 + resolucao2)
 }
 
+// ResolucaoNaPraticaExercicio5 demonstrates a simple function that assigns a string
+// to a variable and then formats it using the FormatResolucaoExercicios function
+// from the format package. The string assigned is "Aprenda Go!" which means "Learn Go!" in Portuguese.
 func ResolucaoNaPraticaExercicio5() {
 	v1 := `Aprenda Go!`
 
 	format.FormatResolucaoExercicios(v1)
 }
 
+// ResolucaoNaPraticaExercicio6 demonstrates the use of the iota identifier in Go.
+// It defines a constant block where the first constant 'ano' is set to 2024, and
+// subsequent constants 'v1', 'v2', 'v3', and 'v4' are automatically incremented by 1
+// starting from 2025. The function then formats these values into a string and
+// passes it to the FormatResolucaoExercicios function for further processing.
 func ResolucaoNaPraticaExercicio6() {
 	const (
 		ano = 2024 + iota
@@ -66,6 +104,18 @@ func ResolucaoNaPraticaExercicio6() {
 	format.FormatResolucaoExercicios(resolucao)
 }
 
+// RespondaAProva is a function that simulates a quiz with multiple-choice questions.
+// It initializes a set of questions and their corresponding correct answers, creates a questionnaire,
+// collects responses from the user, and validates the responses against the correct answers.
+//
+// The function performs the following steps:
+// 1. Initializes a slice of questions (questionario) with their respective options.
+// 2. Initializes a slice of correct answers (gabarito) corresponding to the questions.
+// 3. Creates instances of QuestionnaireService and AnswerService.
+// 4. Uses the QuestionnaireService to create a questionnaire from the list of questions.
+// 5. Uses the AnswerService to collect responses from the user for the created questionnaire.
+// 6. If there is an error during the collection of responses, it prints an error message.
+// 7. Validates the collected responses against the correct answers using the AnswerService.
 func RespondaAProva() {
 	questionario := []format.Questionnaire{
 		{Number: "1.", Question: "Ao utilizar o opeardor curto de declaração, não é necessário especificar o tipo", Options: "[1] Verdadeiro [2] Falso: "},
--- internal/exercicios_ninja_nivel_2/topics.go
+++ internal/exercicios_ninja_nivel_2/topics.go
@@ -1,3 +1,8 @@
+// Package exercicios_ninja_nivel_2 provides functions to execute and display
+// exercises for the Ninja Level 2 course. It includes functions to execute
+// specific sections of the exercises, display a menu of exercise options,
+// and provide help descriptions for each exercise. The package relies on
+// the format package for formatting and displaying sections and help information.
 package exercicios_ninja_nivel_2
 
 import (
@@ -6,8 +11,17 @@ import (
 	"github.com/fabianoflorentino/aprendago/pkg/format"
 )
 
-const rootDir = "internal/exercicios_ninja_nivel_2"
+// rootDir represents the relative path to the directory containing
+// the exercises for Ninja Level 2 in the "aprendago" project.
+// This constant is used to reference the directory where the
+// specific exercise files are stored within the internal package.
+const (
+	rootDir = "internal/exercicios_ninja_nivel_2"
+)
 
+// ExerciciosNinjaNivel2 prints a header for the Ninja Level 2 exercises
+// and sequentially executes sections labeled from "Na prática exercício #1" to "Na prática exercício #7".
+// Each section is executed by calling the executeSection function with the respective section label as an argument.
 func ExerciciosNinjaNivel2() {
 	fmt.Printf("\n\n05 - Exercicios: Ninja Nível 2\n")
 
@@ -20,6 +34,25 @@ func ExerciciosNinjaNivel2() {
 	executeSection("Na prática exercício #7")
 }
 
+// MenuExerciciosNinjaNivel2 returns a slice of format.MenuOptions for various exercises
+// in "Ninja Level 2". Each menu option includes a command-line option string and an
+// associated function to execute when that option is selected.
+//
+// The available options are:
+// - "--na-pratica-exercicio-1 --nivel-2": Executes the section "Na prática exercício #1".
+// - "--na-pratica-exercicio-1 --nivel-2 --resolucao": Executes the resolution function for "Na prática exercício #1".
+// - "--na-pratica-exercicio-2 --nivel-2": Executes the section "Na prática exercício #2".
+// - "--na-pratica-exercicio-2 --nivel-2 --resolucao": Executes the resolution function for "Na prática exercício #2".
+// - "--na-pratica-exercicio-3 --nivel-2": Executes the section "Na prática exercício #3".
+// - "--na-pratica-exercicio-3 --nivel-2 --resolucao": Executes the resolution function for "Na prática exercício #3".
+// - "--na-pratica-exercicio-4 --nivel-2": Executes the section "Na prática exercício #4".
+// - "--na-pratica-exercicio-4 --nivel-2 --resolucao": Executes the resolution function for "Na prática exercício #4".
+// - "--na-pratica-exercicio-5 --nivel-2": Executes the section "Na prática exercício #5".
+// - "--na-pratica-exercicio-5 --nivel-2 --resolucao": Executes the resolution function for "Na prática exercício #5".
+// - "--na-pratica-exercicio-6 --nivel-2": Executes the section "Na prática exercício #6".
+// - "--na-pratica-exercicio-6 --nivel-2 --resolucao": Executes the resolution function for "Na prática exercício #6".
+// - "--na-pratica-exercicio-7 --nivel-2": Executes the section "Na prática exercício #7".
+// - "--na-pratica-exercicio-7 --nivel-2 --prova": Executes the function to respond to the test for "Na prática exercício #7".
 func MenuExerciciosNinjaNivel2([]string) []format.MenuOptions {
 	return []format.MenuOptions{
 		{Options: "--na-pratica-exercicio-1 --nivel-2", ExecFunc: func() { executeSection("Na prática exercício #1") }},
@@ -39,6 +72,11 @@ func MenuExerciciosNinjaNivel2([]string) []format.MenuOptions {
 	}
 }
 
+// HelpMeExerciciosNinjaNivel2 provides a list of available commands and their descriptions
+// for the "Exercícios Ninja - Nível 2" section. Each command is represented by a flag and
+// a description, indicating what the command does. The function prints out the chapter title
+// and then uses the format.PrintHelpMe function to display the list of commands and their
+// respective descriptions.
 func HelpMeExerciciosNinjaNivel2() {
 	hlp := []format.HelpMe{
 		{Flag: "--na-pratica-exercicio-1 --nivel-2", Description: "Apresenta o primeiro exercício prático do nível 2.", Width: 0},
@@ -61,6 +99,10 @@ func HelpMeExerciciosNinjaNivel2() {
 	format.PrintHelpMe(hlp)
 }
 
+// executeSection formats and processes a given section of the project.
+// It takes a section name as a string parameter and uses the FormatSection
+// function from the format package to apply formatting to the specified section
+// within the root directory.
 func executeSection(section string) {
 	format.FormatSection(rootDir, section)
 }
