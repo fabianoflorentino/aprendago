ba83e01fad6325d8238d537560775613f00dad8e
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-12-03 09:49:41 -0300

    feat: add detailed documentation for functions in the exercicios_ninja_nivel_5 package

--- internal/exercicios_ninja_nivel_5/resolution_exercises.go
+++ internal/exercicios_ninja_nivel_5/resolution_exercises.go
@@ -1,7 +1,15 @@
+// Package exercicios_ninja_nivel_5 contains solutions for practical exercises
+// in level 5 of the Ninja Go course. The exercises cover various Go language
+// features such as structs, slices, maps, and anonymous structs.
 package exercicios_ninja_nivel_5
 
 import "fmt"
 
+// ResolucaoNaPraticaExercicio1 demonstrates the creation and usage of a custom struct type in Go.
+// The function defines a 'pessoa' struct with fields for first name, last name, and favorite ice cream flavors.
+// It then creates two instances of 'pessoa', each with different values for these fields.
+// These instances are stored in a slice of 'pessoa'.
+// The function iterates over the slice and prints out the first name, last name, and favorite ice cream flavors of each person.
 func ResolucaoNaPraticaExercicio1() {
 	type pessoa struct {
 		nome               string
@@ -31,6 +39,10 @@ func ResolucaoNaPraticaExercicio1() {
 	}
 }
 
+// ResolucaoNaPraticaExercicio2 demonstrates the creation and usage of a custom struct type in Go.
+// It defines a struct type `pessoa` with fields for first name, last name, and favorite ice cream flavors.
+// Two instances of `pessoa` are created and stored in a map, with the last name as the key.
+// The function then iterates over the map and prints each person's last name followed by their favorite ice cream flavors.
 func ResolucaoNaPraticaExercicio2() {
 	type pessoa struct {
 		nome               string
@@ -63,6 +75,14 @@ func ResolucaoNaPraticaExercicio2() {
 	}
 }
 
+// ResolucaoNaPraticaExercicio3 demonstrates the use of embedded structs in Go.
+// It defines a base struct `veiculo` with fields `portas` (doors) and `cor` (color).
+// Two other structs, `caminhonete` (pickup truck) and `sedan`, embed the `veiculo` struct
+// and add their own fields: `tracaoNasQuatro` (four-wheel drive) for `caminhonete` and
+// `modeloLuxo` (luxury model) for `sedan`.
+// The function creates instances of `caminhonete` and `sedan`, initializes them with values,
+// and prints their details using the `fmt.Printf` function. It also demonstrates accessing
+// the embedded struct fields directly.
 func ResolucaoNaPraticaExercicio3() {
 	type veiculo struct {
 		portas int
@@ -104,6 +124,14 @@ func ResolucaoNaPraticaExercicio3() {
 	fmt.Println("Cor do sedan:", sdn.cor)
 }
 
+// ResolucaoNaPraticaExercicio4 demonstrates the creation and initialization of an anonymous struct
+// representing a person with fields for name, age, friends, and favorite foods. The function then
+// prints the struct to the console. The struct includes:
+// - Nome: a string representing the person's name
+// - Idade: an integer representing the person's age
+// - Amigos: a map where the keys are friends' names (strings) and the values are their ages (integers)
+// - ComidasFavoritas: a slice of strings representing the person's favorite foods
+// The function also contains commented-out code for converting the struct to JSON and printing it.
 func ResolucaoNaPraticaExercicio4() {
 	pessoa := struct {
 		Nome             string
--- internal/exercicios_ninja_nivel_5/topics.go
+++ internal/exercicios_ninja_nivel_5/topics.go
@@ -1,3 +1,8 @@
+// Package exercicios_ninja_nivel_5 provides functions to execute and display
+// exercises for Ninja Level 5. It includes functions to present the exercises,
+// display their resolutions, and provide help information for the exercises.
+// The package utilizes the format package for formatting and displaying sections
+// and help information.
 package exercicios_ninja_nivel_5
 
 import (
@@ -6,8 +11,18 @@ import (
 	"github.com/fabianoflorentino/aprendago/pkg/format"
 )
 
-const rootDir = "internal/exercicios_ninja_nivel_5"
+// rootDir represents the relative path to the directory where the exercises
+// for Ninja Level 5 are stored within the project. This constant is used to
+// reference the directory in a consistent manner throughout the codebase,
+// ensuring that any changes to the directory structure can be managed by
+// updating this single constant.
+const (
+	rootDir = "internal/exercicios_ninja_nivel_5"
+)
 
+// ExerciciosNinjaNivel5 prints the title of Chapter 11 and executes a series of sections
+// labeled as "Na prática: Exercício #1" through "Na prática: Exercício #4". Each section
+// is executed by calling the executeSection function with the respective section title.
 func ExerciciosNinjaNivel5() {
 	fmt.Printf("\n\nCapítulo 11: Exercícios Ninja Nível 5\n")
 
@@ -17,6 +32,10 @@ func ExerciciosNinjaNivel5() {
 	executeSection("Na prática: Exercício #4")
 }
 
+// MenuExerciciosNinjaNivel5 returns a slice of format.MenuOptions for the exercises in level 5.
+// Each MenuOption contains a command-line option string and an associated function to execute.
+// The options include commands for running specific exercises and their resolutions.
+// The function takes a slice of strings as an argument, which can be used to pass additional parameters if needed.
 func MenuExerciciosNinjaNivel5([]string) []format.MenuOptions {
 	return []format.MenuOptions{
 		{Options: "--na-pratica-exercicio-1 --nivel-5", ExecFunc: func() { executeSection("Na prática: Exercício #1") }},
@@ -30,6 +49,10 @@ func MenuExerciciosNinjaNivel5([]string) []format.MenuOptions {
 	}
 }
 
+// HelpMeExerciciosNinjaNivel5 provides a list of help commands for the exercises in Ninja Level 5.
+// It creates a slice of HelpMe structs, each containing a flag and its description.
+// The flags correspond to different practical exercises and their resolutions in Level 5.
+// The function then prints the chapter title and uses the format.PrintHelpMe function to display the help information.
 func HelpMeExerciciosNinjaNivel5() {
 	hlp := []format.HelpMe{
 		{Flag: "--na-pratica-exercicio-1 --nivel-5", Description: "Apresenta o primeiro exercício prático do Nível 5.", Width: 0},
@@ -46,6 +69,10 @@ func HelpMeExerciciosNinjaNivel5() {
 	format.PrintHelpMe(hlp)
 }
 
+// executeSection formats and processes a given section of the project.
+// It takes a section name as a string parameter and uses the FormatSection
+// function from the format package to apply formatting to the specified section
+// within the root directory.
 func executeSection(section string) {
 	format.FormatSection(rootDir, section)
 }
