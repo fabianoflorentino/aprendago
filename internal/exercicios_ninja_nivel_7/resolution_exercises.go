df9d4bb5d0494a8b6c4dd19b5ff8edb23a53b844
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-12-03 09:50:01 -0300

    feat: add detailed documentation for functions in the exercicios_ninja_nivel_7 package

--- internal/exercicios_ninja_nivel_7/resolution_exercises.go
+++ internal/exercicios_ninja_nivel_7/resolution_exercises.go
@@ -1,12 +1,32 @@
+// Package exercicios_ninja_nivel_7 contains solutions for practical exercises
+// at level 7 of the Ninja Go course. It includes functions demonstrating
+// basic Go concepts such as pointers, structs, and function calls.
 package exercicios_ninja_nivel_7
 
 import "fmt"
 
+// pessoa represents a person with a first name, last name, and age.
+// Fields:
+// - nome: the first name of the person
+// - sobrenome: the last name of the person
+// - idade: the age of the person
+type pessoa struct {
+	nome      string
+	sobrenome string
+	idade     int
+}
+
+// ResolucaoNaPraticaExercicio1 prints the memory address of a string variable.
+// It initializes a string variable 'valor' with the value "valor" and then
+// prints the memory address of this variable using the built-in println function.
 func ResolucaoNaPraticaExercicio1() {
 	valor := "valor"
 	println(&valor)
 }
 
+// ResolucaoNaPraticaExercicio2 demonstrates the creation and modification of a 'pessoa' struct instance.
+// It initializes a 'pessoa' struct with predefined values, prints the original values,
+// then calls the 'mudeMe' function to modify the struct via a pointer, and finally prints the modified values.
 func ResolucaoNaPraticaExercicio2() {
 	p := pessoa{
 		nome:      "Fulano",
@@ -21,12 +41,13 @@ func ResolucaoNaPraticaExercicio2() {
 	fmt.Printf("Alterado -> Nome: %s %s, Idade: %d\n", p.nome, p.sobrenome, p.idade)
 }
 
-type pessoa struct {
-	nome      string
-	sobrenome string
-	idade     int
-}
-
+// mudeMe modifies the fields of the given pessoa struct.
+// It sets the nome field to "João", the sobrenome field to "Silva",
+// and the idade field to 30.
+//
+// Parameters:
+//
+//	p (*pessoa): A pointer to a pessoa struct that will be modified.
 func mudeMe(p *pessoa) {
 	p.nome = "João"
 	p.sobrenome = "Silva"
--- internal/exercicios_ninja_nivel_7/topics.go
+++ internal/exercicios_ninja_nivel_7/topics.go
@@ -1,3 +1,6 @@
+// Package exercicios_ninja_nivel_7 provides functions and utilities for executing and displaying
+// exercises from Chapter 15: Ninja Level 7. It includes functions to execute specific exercises,
+// display menu options, and provide help descriptions for the exercises.
 package exercicios_ninja_nivel_7
 
 import (
@@ -6,8 +9,17 @@ import (
 	"github.com/fabianoflorentino/aprendago/pkg/format"
 )
 
-const rootDir = "internal/exercicios_ninja_nivel_7"
+// rootDir is a constant that holds the relative path to the directory
+// containing the exercises for Ninja Level 7 within the project.
+// This path is used to reference the location of the exercises
+// and manage file operations related to them.
+const (
+	rootDir = "internal/exercicios_ninja_nivel_7"
+)
 
+// ExerciciosNinjaNivel7 prints the title for Chapter 15: Exercícios Ninja Nível 7
+// and executes the sections for the exercises. It calls the executeSections function
+// with the titles of the exercises as arguments.
 func ExerciciosNinjaNivel7() {
 	fmt.Printf("\n\nCapítulo 15: Exercícios Ninja Nível 7\n")
 
@@ -15,6 +27,13 @@ func ExerciciosNinjaNivel7() {
 	executeSections("Na prática: exercício #2")
 }
 
+// MenuExercicioNinjaNivel7 returns a slice of format.MenuOptions for the exercises in Ninja Level 7.
+// Each MenuOption contains a command-line option string and a corresponding function to execute.
+// The available options are:
+// --na-pratica-exercicio-1 --nivel-7: Executes the first practical exercise of level 7.
+// --na-pratica-exercicio-1 --nivel-7 --resolucao: Executes the resolution function for the first practical exercise of level 7.
+// --na-pratica-exercicio-2 --nivel-7: Executes the second practical exercise of level 7.
+// --na-pratica-exercicio-2 --nivel-7 --resolucao: Executes the resolution function for the second practical exercise of level 7.
 func MenuExercicioNinjaNivel7([]string) []format.MenuOptions {
 	return []format.MenuOptions{
 		{Options: "--na-pratica-exercicio-1 --nivel-7", ExecFunc: func() { executeSections("Na prática: exercício #1") }},
@@ -24,6 +43,9 @@ func MenuExercicioNinjaNivel7([]string) []format.MenuOptions {
 	}
 }
 
+// HelpMeExerciciosNinjaNivel7 prints a help message for the exercises of Ninja Level 7.
+// It creates a slice of HelpMe structs, each containing a flag and its description,
+// and then prints the help message for each exercise and its resolution.
 func HelpMeExerciciosNinjaNivel7() {
 	hlp := []format.HelpMe{
 		{Flag: "--na-pratica-exercicio-1 --nivel-7", Description: "Apresenta o primeiro exercício prático do Nível 7.", Width: 0},
@@ -36,6 +58,12 @@ func HelpMeExerciciosNinjaNivel7() {
 	format.PrintHelpMe(hlp)
 }
 
+// executeSections formats and processes a specific section of the project.
+// It takes a section name as a parameter and applies formatting to that section
+// using the FormatSection function from the format package.
+//
+// Parameters:
+//   - section: A string representing the name of the section to be formatted.
 func executeSections(section string) {
 	format.FormatSection(rootDir, section)
 }
