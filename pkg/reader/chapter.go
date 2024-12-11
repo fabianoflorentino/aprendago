cd6c50c3317d5423e17737683e59bfe614b0a834
Daniel Moreto <dfmoreto@gmail.com>
2024-12-11 09:06:33 -0300

    refact: update package reader/chapter to use new entities package

--- pkg/reader/chapter.go
+++ pkg/reader/chapter.go
@@ -5,33 +5,22 @@ import (
 	"path/filepath"
 	"strconv"
 
+	"github.com/fabianoflorentino/aprendago/internal/entities"
 	"github.com/fabianoflorentino/aprendago/pkg/logger"
-	"gopkg.in/yaml.v2"
+	"gopkg.in/yaml.v3"
 )
 
-type Topic struct {
-	Flag    string `yaml:"flag"`
-	Title   string `yaml:"title"`
-	HelpMe  string `yaml:"help_me"`
-	Outline string `yaml:"outline"`
-}
-
-type Chapter struct {
-	Title  string  `yaml:"title"`
-	Topics []Topic `yaml:"topics"`
-}
-
 const (
-	CHAPTER_ROOT_DIR    = "./internal/outlines"
+	CHAPTER_ROOT_DIR    = "internal/outlines"
 	CHAPTER_ROOT_NAME   = "capitulo"
 	CHAPTER_YML_FORMAT  = ".yml"
 	CHAPTER_YAML_FORMAT = ".yaml"
 )
 
 // ReadChapterFile é utilizada para ler o arquivo YML do capítulo e retornar uma estrutura de Chapter
-func ReadChapterFile(chapterNumber int) (Chapter, error) {
+func ReadChapterFile(chapterNumber int) (entities.Chapter, error) {
 	var docPath string
-	var chapter Chapter
+	var chapter entities.Chapter
 
 	docPathYML := filepath.Join(CHAPTER_ROOT_DIR, CHAPTER_ROOT_NAME+strconv.Itoa(chapterNumber)+CHAPTER_YML_FORMAT)
 	docPathYAML := filepath.Join(CHAPTER_ROOT_DIR, CHAPTER_ROOT_NAME+strconv.Itoa(chapterNumber)+CHAPTER_YAML_FORMAT)
@@ -42,17 +31,17 @@ func ReadChapterFile(chapterNumber int) (Chapter, error) {
 		docPath = docPathYAML
 	} else {
 		logger.Log("File %s or %s not found", docPathYML, docPathYAML)
-		return Chapter{}, os.ErrNotExist
+		return entities.Chapter{}, os.ErrNotExist
 	}
 
 	data, err := os.ReadFile(docPath)
 	if err != nil {
-		return Chapter{}, err
+		return entities.Chapter{}, err
 	}
 
 	err = yaml.Unmarshal(data, &chapter)
 	if err != nil {
-		return Chapter{}, err
+		return entities.Chapter{}, err
 	}
 
 	return chapter, nil
