0d93fe0187d202545948a6c4437c9e86ec1f6424
Daniel Moreto <dfmoreto@gmail.com>
2024-12-11 09:07:34 -0300

    refact: change chapter structure It was changed to have substructures: Outlines, Exercises and Exam

--- pkg/format/chapter.go
+++ pkg/format/chapter.go
@@ -8,17 +8,18 @@ import (
 
 // PrintChapterHelpMe imprime o capítulo conforme o número recebido
 func PrintChapterHelpMe(chapterNumber int) {
-	chapter, error := reader.ReadChapterFile(chapterNumber)
-	if error != nil {
-		fmt.Printf("Erro ao tentar extrair dados do Capítulo %s\n", chapterNumber)
+	chapter, err := reader.ReadChapterFile(chapterNumber)
+	if err != nil {
+		fmt.Printf("Erro ao tentar extrair dados do Capítulo %d:\n%s", chapterNumber, err)
 		return
 	}
 
 	helpMes := []HelpMe{}
 
-	for _, topic := range chapter.Topics {
-		helpMe := HelpMe{Flag: topic.Flag, Description: topic.HelpMe, Width: 0}
-		helpMes = append(helpMes, helpMe)
+	for _, topic := range chapter.Topics() {
+		for _, helpMe := range chapter.SerializeHelpMes(topic) {
+			helpMes = append(helpMes, HelpMe{Flag: helpMe["flag"], Description: helpMe["span"], Width: 0})
+		}
 	}
 
 	fmt.Printf("\nCapitulo %d: %s\n", chapterNumber, chapter.Title)
