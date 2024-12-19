3fd1bf2c02b1f63e892ea7a7bc351d9eb0cd9ec5
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-12-18 21:06:22 -0300

    feat: ajustar formatação e clareza nas instruções do exercício 5

--- internal/exercicios_ninja_nivel_11/resolution_exercises.go
+++ internal/exercicios_ninja_nivel_11/resolution_exercises.go
@@ -91,10 +91,10 @@ func ResolucaoNaPraticaExercicio5() {
 	var content = `
 - Nos capítulos seguintes, uma das coisas que veremos é testes.
 - Para testar sua habilidade de se virar por conta própria... desafio:
-		- Utilizando as seguintes fontes: https://godoc.org/testing & http://www.golang-book.com/books/intro/12
-		- Tente descobrir por conta própria como funcionam testes em Go.
-		- Pode usar tradutor automático, pode rodar código na sua máquina, pode procurar no Google. Vale tudo.
-		- O exercício é: crie um teste simples de uma função ou método ou pedaço qualquer de código.
+  - Utilizando as seguintes fontes: https://godoc.org/testing & http://www.golang-book.com/books/intro/12
+  - Tente descobrir por conta própria como funcionam testes em Go.
+  - Pode usar tradutor automático, pode rodar código na sua máquina, pode procurar no Google. Vale tudo.
+  - O exercício é: crie um teste simples de uma função ou método ou pedaço qualquer de código.
 `
 	resolution := "internal/exercicios_ninja_nivel_11/resolution_exercises_test.go"
 
