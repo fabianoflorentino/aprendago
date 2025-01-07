34b994b386355fe5330339307a828b24e137595e
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2025-01-07 19:27:20 -0300

    feat: refactor tests from exercises 1 and 2 to capture the output and verify the expected formating

--- internal/exercicios_ninja_nivel_4/resolution_exercises_test.go
+++ internal/exercicios_ninja_nivel_4/resolution_exercises_test.go
@@ -5,7 +5,10 @@
 package exercicios_ninja_nivel_4
 
 import (
+	"bytes"
 	"fmt"
+	"io"
+	"os"
 	"strings"
 	"testing"
 )
@@ -21,21 +24,71 @@ const (
 // concatenating each integer to a string with a comma and space separator, and comparing the result
 // to an expected string. If the resulting string does not match the expected string, the test fails.
 func TestResolucaoNaPraticaExercicio1(t *testing.T) {
-	var resolution string
-	array := [5]int{1, 2, 3, 4, 5}
+	var output string
 
-	for _, v := range array {
-		if v == array[len(array)-1] {
-			resolution += fmt.Sprintf("%d", v)
-			continue
-		}
-		resolution += fmt.Sprintf("%d, ", v)
+	func() {
+		defer func() {
+			if rr := recover(); rr != nil {
+				output = fmt.Sprint(rr)
+			}
+		}()
+
+		output = captureOutput(ResolucaoNaPraticaExercicio1)
+	}()
+
+	expect := `
+Resolução:
+1, 2, 3, 4, 5
+`
+	if !strings.Contains(strings.TrimSpace(output), strings.TrimSpace(expect)) {
+		t.Errorf(expectTemplate, expect, output)
 	}
-	resolution = strings.TrimSpace(resolution)
+}
 
-	expect := "1, 2, 3, 4, 5"
+// TestResolucaoNaPraticaExercicio2 tests the function ResolucaoNaPraticaExercicio2
+// by capturing its output and comparing it to the expected output. It uses a
+// deferred function to recover from any panic that might occur during the execution
+// of ResolucaoNaPraticaExercicio2 and captures the panic message if it happens.
+// The test checks if the captured output contains the expected string, ignoring
+// leading and trailing whitespace, and reports an error if it does not match.
+func TestResolucaoNaPraticaExercicio2(t *testing.T) {
+	var output string
 
-	if resolution != expect {
-		t.Errorf(expectTemplate, expect, resolution)
+	func() {
+		defer func() {
+			if r := recover(); r != nil {
+				output = fmt.Sprint(r)
+			}
+		}()
+		output = captureOutput(ResolucaoNaPraticaExercicio2)
+	}()
+
+	expect := `
+Resolução:
+
+Slice: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+Tipo: []string
+`
+
+	if !strings.Contains(strings.TrimSpace(output), strings.TrimSpace(expect)) {
+		t.Errorf(expectTemplate, expect, output)
 	}
+}
+
+// captureOutput captures and returns the output of a function that writes to the standard output.
+// It temporarily redirects os.Stdout to a pipe, executes the provided function, and then restores os.Stdout.
+// The captured output is returned as a string.
+func captureOutput(resolutionFunction func()) string {
+	read, write, _ := os.Pipe()
+	stdout := os.Stdout
+	os.Stdout = write
+
+	resolutionFunction()
+
+	write.Close()
+	os.Stdout = stdout
+
+	var buf bytes.Buffer
+	io.Copy(&buf, read)
+	return buf.String()
 }
