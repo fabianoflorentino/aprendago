78d686330acb86ae7573b90b559589cf6154b023
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2025-01-27 22:56:18 -0300

    test: add tests for ResolucaoNaPraticaExercicio1 in nível 9

---
+++ internal/exercicios_ninja_nivel_9/resolution_exercises_test.go
@@ -0,0 +1,37 @@
+// Package exercicios_ninja_nivel_9 contains tests and exercises for level 9 of the Ninja Go course.
+// This package includes various functions and tests to practice advanced Go programming concepts.
+package exercicios_ninja_nivel_9
+
+import (
+	"strconv"
+	"strings"
+	"testing"
+
+	"github.com/fabianoflorentino/aprendago/pkg/logger"
+	"github.com/fabianoflorentino/aprendago/pkg/output"
+)
+
+// expectTemplate is a format string used to display the expected and actual values
+// in a test output. It helps in comparing the expected result with the actual result
+// by formatting them in a readable way.
+const (
+	expectTemplate = "\nwant:\n%s\n\ngot:\n%s\n"
+)
+
+// TestResolucaoNaPraticaExercicio1 tests the function ResolucaoNaPraticaExercicio1
+// by capturing its output and checking if the number of occurrences of the word "Goroutine"
+// matches the expected count. If the output does not match the expected count, the test fails.
+func TestResolucaoNaPraticaExercicio1(t *testing.T) {
+	output := output.New()
+	result, err := output.Capture(ResolucaoNaPraticaExercicio1)
+	if err != nil {
+		logger.Log("Failed to capture output: %v", err)
+		t.Fatalf("unexpected error: %v", err)
+	}
+
+	expect := 10
+
+	if strings.Count(result, "Goroutine") != expect {
+		t.Errorf(expectTemplate, strconv.Itoa(expect), result)
+	}
+}
