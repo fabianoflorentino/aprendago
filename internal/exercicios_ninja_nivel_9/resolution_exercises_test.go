de5c0bd4b61bbafdb06c96de4d390eadd13ca5d7
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2025-01-28 22:17:02 -0300

    test: add test for ResolucaoNaPraticaExercicio2 with output validation

--- internal/exercicios_ninja_nivel_9/resolution_exercises_test.go
+++ internal/exercicios_ninja_nivel_9/resolution_exercises_test.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/fabianoflorentino/aprendago/pkg/logger"
 	"github.com/fabianoflorentino/aprendago/pkg/output"
+	"github.com/fabianoflorentino/aprendago/pkg/trim"
 )
 
 // expectTemplate is a format string used to display the expected and actual values
@@ -33,5 +34,27 @@ func TestResolucaoNaPraticaExercicio1(t *testing.T) {
 
 	if strings.Count(result, "Goroutine") != expect {
 		t.Errorf(expectTemplate, strconv.Itoa(expect), result)
+	}
+}
+
+// TestResolucaoNaPraticaExercicio2 tests the function ResolucaoNaPraticaExercicio2
+// by capturing its output and comparing it to the expected string.
+// It fails the test if the output does not contain the expected string.
+func TestResolucaoNaPraticaExercicio2(t *testing.T) {
+	output := output.New()
+	result, err := output.Capture(ResolucaoNaPraticaExercicio2)
+	if err != nil {
+		logger.Log("Failed to capture output: %v", err)
+		t.Fatalf("unexpected error: %v", err)
+	}
+
+	expect := `
+Olá, meu nome é fabiano
+`
+
+	trim := trim.New()
+
+	if !strings.Contains(trim.String(result), trim.String(expect)) {
+		t.Errorf(expectTemplate, expect, result)
 	}
 }
