d43bc12024adb635088f7b457298a9f5ba4e5c67
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2025-04-01 13:41:57 -0300

    refactor: streamline Topics function and update topic list to use camelCase naming convention

--- internal/aplicacoes/topics.go
+++ internal/aplicacoes/topics.go
@@ -15,49 +15,16 @@ import (
 func Topics() {
 	fmt.Printf("\nCapítulo 16: Aplicações\n")
 
-	content := topic.New()
-	contentsAplicacoes(rootDir, content)
-}
-
-// contentsAplicacoes initializes and populates the contents of applications topics.
-// It creates a new instance of topics, retrieves the list of topics for applications,
-// and processes their contents from the specified root directory.
-//
-// Parameters:
-//   - rootDir: The root directory path where the topics' contents are located.
-func contentsAplicacoes(rootDir string, contents topic.ContentsProvider) {
-	contents.TopicsContents(rootDir, listOfTopicsAplicacoes())
-}
-
-// listOfTopicsAplicacoes returns a slice of strings containing a list of topics
-// related to various Go programming concepts and functionalities. The topics
-// include JSON handling, interface usage, sorting, and encryption with bcrypt.
-func listOfTopicsAplicacoes() []string {
 	list := []string{
-		DocumentacaoJSON,
-		JSONMarshal,
-		JSONUnmarshal,
-		InterfaceWriter,
-		PacoteSort,
-		CustomizandoSort,
-		Bcrypt,
+		documentacaoJSON,
+		jsonMarshal,
+		jsonUnmarshal,
+		interfaceWriter,
+		pacoteSort,
+		customizandoSort,
+		bcrypt,
 	}
 
-	return listOfTopics(list)
-}
-
-// listOfTopics takes a slice of strings as input and returns a new slice of strings.
-// The returned slice is initialized with a capacity of 7 and contains all elements
-// from the input slice.
-//
-// Parameters:
-//   - inputList: A slice of strings to be copied into the output slice.
-//
-// Returns:
-//   - A new slice of strings containing all elements from the input slice.
-func listOfTopics(inputList []string) []string {
-	outputList := make([]string, 0, 12)
-	outputList = append(outputList, inputList...)
-
-	return outputList
+	c := topic.New()
+	c.TopicsContents(rootDir, c.ListOfTopics(list, 7))
 }
