f948877119d13ffc7e24dac7b0e3da9686c3e2b6
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2025-04-04 00:29:05 -0300

    test: add unit test for Help function in agrupamento_de_dados package

---
+++ internal/agrupamento_de_dados/help_test.go
@@ -0,0 +1,27 @@
+package agrupamento_de_dados
+
+import (
+	"os"
+	"testing"
+)
+
+func TestHelp(t *testing.T) {
+	var err error
+
+	oldStdout := os.Stdout
+	defer func() { os.Stdout = oldStdout }()
+
+	nullFile, err := os.OpenFile(os.DevNull, os.O_WRONLY, 0)
+	if err != nil {
+		t.Fatalf("failed to open null device: %v", err)
+	}
+	defer nullFile.Close()
+
+	os.Stdout = nullFile
+
+	Help()
+
+	if err != nil {
+		t.Errorf("Help() failed: %v", err)
+	}
+}
